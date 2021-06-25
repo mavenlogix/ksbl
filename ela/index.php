97f372d97141e3b24ad608c6f3d64a702d150039
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-25 21:40:20 +0500

    updated

---
+++ ela/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+
+
+	 <!-- <title> Engro Leadership Academy | ELA - Karachi School of Business and Leadership </title> -->
+  
+
+<?php 
+
+include('../partials/head.php');
+
+?>
+
+<body class="mba ela full-height">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <!--div class="header-video">
+			<img src="../images/ela/header.jpg" alt="ELA"/>
+			</div-->
+        <div class="container">
+             <?php
+                include('../partials/navbar.php'); 
+            ?>
+            <div class="header-content">
+                <a href="../index.html" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>ENGRO LEADERSHIP ACADEMY</h1>
+                </div>
+                <!-- container -->
+            </div>
+        </div><!-- container -->
+        <div class="header-bottom">
+            <div class="ksbl-programs-wrap">
+                <div class="container">
+                    <div class="ksbl-programs">
+                        <a href="../mba/" class="ksbl-program mba">MBA</a>
+                        <!-- mba -->
+                        <a href="../emba/" class="ksbl-program executive-mba">Executive MBA</a>
+                        <!-- executive-mba -->
+                        <a href="../ms-business-analytics/" class="ksbl-program engro-leadership-academy ">MS Business Analytics</a>
+                        <a href="../executive-education-services/" class="ksbl-program executive-edu-mba active">Executive Education</a>
+                        <!-- executive-edu-mba -->
+                        <!-- <li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li> -->
+                        <!-- engro-leadership-academy	-->
+                    </div><!-- container -->
+                </div>
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                       MBA Department                             #
+	#----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../executive-education-services/" class="department" title="MBA">About</a>
+            <!-- About Executive Education -->
+            <a href="../executive-education-services/customized-programs/" class="department" title="Custom Programs">Custom Programs</a>
+            <!-- Custom Programs -->
+            <a href="../ela/" class="department active" title="Engro Leadership Academy">Engro Leadership Academy</a>
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+	#                   MBA Department END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      ELA Start                           #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../images/ela/inaug.jpg" alt="Inauguration" />
+            </div>
+            <div class="left-side">
+                <h2>Engro Leadership Academy (ELA)</h2>
+                <div class="detail">
+                    <p>ELA is a structured academia-industry collaboration offering leadership development and capability building at scale for private and public-sector institutions, developed in partnership with Engro Corporation. The program offers a McKinsey designed immersive 9-month leadership program has been designed following the global business vision to promote character building and improved decision-making capacity. Each program consists of 3 elements; first having a total of four boot camps, each comprising 3 days; second, reinforcement through practical fieldwork and third - individual learning and development paths. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      ELA END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      About ELA Start                           #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side"> <img src="../images/ela/ela_ksbl.jpg" alt="ELA KSBL" /> </div>
+            <div class="right-side">
+                <h2>ELA Vision & Mission</h2>
+                <div class="detail">
+                    <p>In line with its vision, the Engro Leadership Academy provides leadership training to current & future Engro leaders while endeavouring to promote managerial skills and capacity development. Tailored training program for all participating, the training is conducted at a dedicated facility inside the KSBL campus.
+                        <br><br>This exquisite program is a perfect blend of action-based learning, web-based modules & field intervention, delivered by effective faculty with hands-on local as well as global experience. In the long term, the successful implementation of the Engro Leadership Academy is a stepping stone for KSBL to partner up with further large corporations of Pakistan.
+                    </p>
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      About ELA END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      Open House Start                            #
+	#----------------------------------------------------------------->
+    <div class="open-house-section">
+        <div class="container">
+            <div class="user-detail">
+                <div class="user-info">
+                    <div class="detail" align="center">
+                        <br>
+                        <h2>ELA at a Glance</h2><br>
+                        The purpose-driven Engro Leadership Academy is a firm-wide training that fosters leadership capabilities, knowledge and humility to all managerial employees, with an emphasis on a deep reflection around thinking-pattern, moral belief, value system and fear factors.
+                        <br><br>The content and curriculum designed for each of ELA's cohorts are delivered by world-class facilitators aimed at senior managers while KSBL facilitators take the lead for mind or junior-level managers. The main crux of the training is to empower managers at any organizational level to lead themselves, others, organization and change.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      About MBA END                              #
+	#----------------------------------------------------------------->
+      <!-- footer start -->
+    <?php
+        include('./partials/footer.php');
+    ?>
+    <!-- footer end -->
+   <?php
+        include('./partials/footer-scripts.php');
+    ?>
+</body>
+
+</html>
