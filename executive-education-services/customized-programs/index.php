e259febc7a936503090bdeb0c2850997a526e73e
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 14:40:22 +0500

    updated

---
+++ executive-education-services/customized-programs/index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- <title>KSBL | Custom Programs</title> -->
+<?php 
+
+include('.../partials/head.php');
+
+?>
+
+<body class="mba ees cust-prgm">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <!--div class="header-video">
+			<img src="../../images/ees/custom-programs.jpg" alt="Customized Programs"/>
+			</div-->
+        <div class="container">
+            <?php
+include('.../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../../index.html" title="KSBL" class="logo">
+                    <img src="../../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>CUSTOM PROGRAMS</h1>
+                    <h2>Scultping existing and potential leaders with customized solutions</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        <div class="header-bottom">
+            <div class="ksbl-programs-wrap">
+                <div class="container">
+                    <div class="ksbl-programs">
+                        <a href="../../mba/" class="ksbl-program mba">MBA</a>
+                        <!-- mba -->
+                        <a href="../../emba/" class="ksbl-program executive-mba">Executive MBA</a>
+                        <!-- executive-mba -->
+                        <a href="../../ms-business-analytics/" class="ksbl-program engro-leadership-academy ">MS Business Analytics</a>
+                        <a href="../../executive-education-services/" class="ksbl-program executive-edu-mba active">Executive Education</a>
+                        <!-- executive-edu-mba -->
+                        <!--a href="../../ela/" class="ksbl-program engro-leadership-academy">Engro Leadership Academy</a-->
+                        <!-- engro-leadership-academy -->
+                    </div><!-- container -->
+                </div>
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                       MBA Department                             #
+	#----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../" class="department" title="MBA">About</a>
+            <!-- About Executive Education -->
+            <a href="../executive-education-services/customized-programs/" class="department active" title="Custom Programs">Custom Programs</a>
+            <!-- Custom Programs -->
+            <a href="../../ela/" class="department" title="Engro Leadership Academy">Engro Leadership Academy</a>
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+	#                   MBA Department END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                       Top-section Start                          #
+	#----------------------------------------------------------------->
+    <div class="mba-section top-section">
+        <div class="container">
+            <div class="right-side">
+                <h2>Custom Programs</h2>
+                <div class="detail">
+                    <p>Our Custom Programs focus on a broader range of skills that enable professionals at all levels to transit to the next stage in their career or business. All business problems might appear the same yet call for individualistic diagnosis and processes to learn and master. The custom programs are offered separately for individuals, organizations and groups and unique to the needs of an individual or organization.</p>
+                    <p>With extensive research on change in leadership behaviour, specializing in the ability to break silos and foster relationships, our bespoke programs are centred around customer needs and sensitivity to an organization's culture. The truly transformational nature of our programs, stemming from both a pre-program learning assessment and post-program follow-ups, makes them a high-impact offering in the market.</p>
+                </div>
+            </div>
+            <div class="left-side">
+                <h2>Tailored to the needs</h2>
+                <div class="detail">
+                    <p>The faculty for our executive education programs is hand-picked and strikes a fine balance between offering the best learning practices and experiences. Being seasoned industry practitioners, this group brings learned wisdom of the market and can integrate the latest thinking with strong academic ideas.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                           Top-section END                         #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#              Financial Assistance Start                          #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/novartis.jpg" alt="Novartis" />
+            </div>
+            <div class="right-side">
+                <h2>Novartis Pharma (Pakistan) Limited</h2>
+                <div class="detail">
+                    <p>KSBL Executive Education conducted a two day Customized program on 'Developing High Performance Teams' for Novartis Pharma (Pakistan) Limited. The program was designed for the company's various teams so that they worked together as a synergized group to achieve Novartis' aspiration of becoming number one pharmaceutical company in 2016. It was attended by 11 participants largely consisting of business heads of different functions including HR, Medical & Regulatory Affairs, Legal & Compliance and Marketing & Sales.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                Financial Assistance END                         #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                    ICI Pakistan Limited Start                    #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/ici-pakistan.jpg" alt="ICI Pakistan Limited" />
+            </div>
+            <div class="right-side">
+                <h2>ICI Pakistan Limited</h2>
+                <div class="detail">
+                    <p>A 3 day Strategy Workshop named Aspiration 2018 was developed and delivered by KSBL in Sep 2013 in Islamabad. The objective of the workshop was to articulate strategy for ICI Pakistan post acquisition. The workshop was conducted by Professor Imran Ali (KSBL) and Dr. Kamal Munir (Judge Business School) and attended by 48 top leaders from ICI. The workshop is believed to have provided the foundation on which ICI Pakistan will not just strengthen its existing portfolio but expand its foot print through diversification and partnerships in new businesses.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                 ICI Pakistan Limited END                         #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#            Aga Khan University Hospital Start                   #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/aga-khan.jpg" alt="Aga Khan University Hospital" />
+            </div>
+            <div class="right-side">
+                <h2>Aga Khan University Hospital</h2>
+                <div class="detail">
+                    <p>KSBL Executive Education Services conducted a customized "Emerging Leaders Development Program" for a diverse cohort of 30 participants from Aga Khan University Hospital comprising of six modules encompassing various management areas including leadership, people management, project management, finance, economics, and strategy. The specialized healthcare industry specific program which commenced in December 2013 and concluded in March 2014 was designed keeping in view the learning and development needs of the participants in each management area. A mix of experiential activities, case studies, interactive discussions, simulations and relevant group discussions enriched the participants' learning experience at KSBL. The program was rated well on all parameters and the participants were content with its design and delivery.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#            Aga Khan University Hospital END                      #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#               United Energy Pakistan (UEP) Start                 #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/uep.jpg" alt="United Energy Pakistan (UEP)" />
+            </div>
+            <div class="right-side">
+                <h2>United Energy Pakistan (UEP)</h2>
+                <div class="detail">
+                    <p>KSBL Executive Education conducted 'Leadership Development Program' consisting of 3 modules for 2 cohorts of emerging leaders from United Energy Pakistan Limited. The modular program laid emphasis on few parameters which are significant to UEPL namely, Value, Expertise, Team Spirit, Decisive Action, Fairness and Transparency and Result Orientation. The program engaged participants in collective understanding of the new work requirements based on collaborative, skillful and result oriented teamwork. Work is underway for the 3rd Cohort scheduled to be delivered in the last quarter of 2015.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#            United Energy Pakistan (UEP) END                      #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                            PARCO Start                           #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/parco.jpg" alt="PARCO" />
+            </div>
+            <div class="right-side">
+                <h2>PARCO</h2>
+                <div class="detail">
+                    <p>KSBL Executive Education initiated a 'Management Development Program' for PARCO and conducted the first module titled 'Leadership & Organisational Performance'. The module included interactive lectures, group activities and simulations to achieve a significant outcome. It was well-attended by 36 high potential leaders from PARCO who discovered their personality types and learnt how to best utilize their traits for leadership, team building and performance management in the organisation to achieve PARCO's growth objectives in future. The program received an excellent rating especially in terms of expected learning outcomes.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                             PARCO END                            #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                            HUBCO Start                           #
+	#----------------------------------------------------------------->
+    <div class="mba-section little">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/hubco.jpg" alt="HUBCO" />
+            </div>
+            <div class="right-side">
+                <h2>HUBCO</h2>
+                <div class="detail">
+                    <p>'Soft Skills for High Potential Leaders' was a three day customized program held for Hub Power Company. Given the importance of soft skills for leadership, KSBL developed a distinct program to help participants develop five key competencies which have been assessed by the world's leading companies as being essential for business success: Adaptive Thinking, Communication, Relationships, Teamwork and Execution.</p>
+                    <p>Hub Power also tasked KSBL to develop another 3 day 'Senior Leadership Development Program' to help transform their functional managers into leaders who could engage their teams, inspire quality performance and set clear direction and strategy. The program facilitated the creation of a pipeline of leaders for future growth objectives and challenges ahead. 13 middle to senior level Managers with technical education and background from various business operations of HUBCO attended the program.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                             HUBCO END                            #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                        K ELECTRIC Start                          #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/k-electric.jpg" alt="K ELECTRIC" />
+            </div>
+            <div class="right-side">
+                <h2>K ELECTRIC</h2>
+                <div class="detail">
+                    <p>K-Electric in collaboration with KSBL initiated and finalized a 'Business Catalyst Program' for a group of 350 participants comprising mainly of Deputy General Managers and Managers. The program aimed to enhance the management and leadership skills of business / function level managers in K-Electric. It was focused on the work environment of the K-Electric management team to ensure that the skills acquired through this initiative be applied to drive future performance. The group was divided into 10 cohorts with each cohort participating for 4 days. Business Catalyst Program was rolled out and delivered during the last quarter of 2014.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                        K ELECTRIC END                            #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                    ENGRO FERTILIZERS Start                       #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/ees/engro.jpg" alt="ENGRO FERTILIZERS" />
+            </div>
+            <div class="right-side">
+                <h2>ENGRO FERTILIZERS</h2>
+                <div class="detail">
+                    <p>KSBL Executive Education is conducting a unique and multi-level 'Business Acumen Program' for Engro Fertilizers Limited. The program consists of three modules, each representing a competency cluster from Engro's Leadership Competency Model. The program started this year with the delivery of the first module in January 2015 on catalyzing business focus, covering the areas of Strategy Formulation and Planning, New Businesses, Global Business Strategy and Financial Statements. The participants gave the program an overall rating of 4.19 out of 5 and look forward to the remaining modules. The second module of the 'Business Acumen Program' is currently being designed and is scheduled for October 2016.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                  ENGRO FERTILIZERS END                           #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                    Welcome Oprtunity Start                       #
+	#----------------------------------------------------------------->
+    <div class="mba-section welcome-oprtunity">
+        <div class="container">
+            <div class="center-side">
+                <h2>We welcome the opportunity to discuss your management education requirements in confidence.</h2>
+                <div class="detail">
+                    <p>For more information please contact our office at<br>
+                        +92 (21) 111 11 KSBL (5725) or PABX: +92 (21) 38771999</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                  ENGRO FERTILIZERS END                           #
+	#----------------------------------------------------------------->
+    <?php
+include('.../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('.../partials/footer-scripts.php');
+?>
+
+</body>
+
+</html>
