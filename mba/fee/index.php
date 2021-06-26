09c67e051f530074b5c11f7dcac99fa35de0cc81
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 19:30:37 +0500

    updated

---
+++ mba/fee/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<!--  <title> MBA Fee Structure - Karachi School of Business & Leadership </title> -->
+<?php 
+
+include('../../partials/head.php');
+
+?>
+
+<body class="mba fee full-height">
+    <!-----------------------------------------------------------------#
+    #                       Header START                               #
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <!--div class="header-video">
+            <img src="../../images/mba/mba_fee.jpg" alt="MBA">
+            </div-->
+        <div class="container">
+                 <?php
+include('../partials/navbar.php'); 
+?>
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>Fee Structure</h1>
+                    <h2>A modular breakdown for all courses</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        <div class="header-bottom">
+            <div class="ksbl-programs-wrap">
+                <div class="container">
+                    <div class="ksbl-programs">
+                        <a href="mba/" class="ksbl-program mba active">MBA</a>
+                        <!-- mba -->
+                        <a href="emba/" class="ksbl-program executive-mba">Executive MBA</a>
+                        <!-- executive-mba -->
+                        <a href="ms-business-analytics/" class="ksbl-program engro-leadership-academy ">MS Business Analytics</a>
+                        <a href="meba/" class="ksbl-program executive-edu-mba ">BS MEBA</a>
+                        <a href="bs-its/" class="ksbl-program executive-edu-mba">BS IT&S</a>
+                        <a href="executive-education-services/" class="ksbl-program executive-edu-mba">Executive Education</a>
+                        <!-- executive-edu-mba -->
+                    </div><!-- container -->
+                </div>
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+    #                       Header END                                 #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #                       MBA Department                             #
+    #----------------------------------------------------------------->
+    <div class="departments add">
+        <div class="container">
+            <a href="../admissions/" class="department" title="Admission Process">Admissions Process</a>
+            <a href="../fee/" class="department active" title="Fees">Fees</a>
+            <a href="../financial-assistance/" class="department" title="Financial Assistance">Financial Assistance</a>
+            <a href="../information-sessions/" class="department" title="Open House">Open House</a>
+            <a href="../../why-KSBL.html" class="department" title="Why KSBL">Why KSBL</a>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                   MBA Department END                             #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #                 Content Section Start                            #
+    #----------------------------------------------------------------->
+    <div class="content-section">
+        <div class="container">
+            <div class="content-area">
+                <h2>Fee Structure 2020-21</h2>
+                <div class="fee-table">
+                    <div class="table-row heading">
+                        <div class="table-col">Fee Listings</div>
+                        <div class="table-col">Amount in PKR</div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-col">* Application Fee</div>
+                        <div class="table-col">5,000 <br>(Inclusive of KMAT fee <br>amounting to 3,000) </div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-col">Per Credit Hour Fee (All inclusive)</div>
+                        <div class="table-col">25,000</div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-col"><br>* Those not attempting KMAT will be required to pay PKR. 2,000 at the time of registration
+                            <br>* KMAT fee includes 2 attempts
+                            <br>* KMAT test date may be rescheduled upon a prior notice of at least 24 hours.
+                            <br>* Application fee is non-refundable.</div>
+                        <div class="table-col"></div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-col"></div>
+                        <div class="table-col"></div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-col"></div>
+                        <div class="table-col"></div>
+                    </div>
+                </div>
+                <div class="payment-table other-charges">
+                    <div class="table-col heading">Mode of Payment</div>
+                    <div class="table-col">
+                        <img src="../../images/mba/payorder.png" alt="Payorder" />
+                        <div class="col-detail">
+                            <h3>Pay Order / Demand Draft</h3>
+                            <div class="other-detail">
+                                All the payments, Pay Order and Demand Draft in favour of Karachi School of Business & Leadership are to be deposited at the Finance department of KSBL
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-col">
+                        <img src="../../images/mba/online-payment.png" alt="Online Payment" />
+                        <div class="col-detail">
+                            <h3>E-Payment </h3>
+                            <div class="other-detail">
+                                All the dues can be paid online to KSBL's official bank account.
+                                <br><strong>Account Title:</strong><br>Karachi School of Business and Leadership
+                                <br><strong>Account number:</strong><br>0010030887260010
+                                <br><strong>IBAN:</strong><br>PK81ABPA0010030887260010
+                                <br><strong>SWIFT:</strong><br>ABPAP
+                                <br><strong>Bank Address:</strong><br>Allied Bank Limited Stadium Road Branch (0971) Karachi
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar">
+                <div class="courses-progremme">
+                    KSBL charges fee per credit hour, applicable as per the total number of credit hours in a Program
+                </div>
+                <div class="grey-area">
+                    <div class="notes">
+                        <div class="tabs">
+                            <div class="tab active" data-tab="notes">Note</div>
+                        </div>
+                        <div class="tabs-content">
+                            <div class="tab-content active" data-tab="notes">
+                                <ul>
+                                    <li>If a student's annual dues surpass PKR 200,000/-, an advance tax of 5% on the whole amount (under section 236 I) will be collected as per the revision in Income Tax Ordinance 2001 by Finance Act 2013</li>
+                                    <li>Advance taxes are adjustable at the time of filing an income tax return. Advance tax certificates will be issued by KSBL at the end of the year</li>
+                                    <li>The fee structure does not include housing, food, government taxes or any other additional facilities</li>
+                                    <li>Fee structure does not vary for international students</li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                        Content Section Start                     #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #                      Open House Start                            #
+    #----------------------------------------------------------------->
+    <div class="open-house-section  new-open-houese">
+        <div class="container">
+            <div class="event-program">
+                <!-- <div class="open-house">
+               Apply Now!                   
+            </div> -->
+                <div class="upcoming-event">
+                    <img src="../../images/mba/meet-team.png" alt="Meet the Team">
+                    <h3>Apply Now! </h3>
+                    <div class="event-detail">
+                        <div class="event-date">
+                            Join KSBL - A growth led
+                            learning system that combines
+                            value-driven leadership and top-grade business skills.
+                        </div>
+                        <div class="event-links">
+                            <a href="http://admissions.colaraz.com/signup?institute=ksbledu" target="_blank" title="Apply Now" class="default-btn">Apply</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-program">
+                <!-- <div class="open-house">
+                   Apply Now!                   
+                </div> -->
+                <div class="upcoming-event contact-us">
+                    <h3>More Information</h3>
+                    <div class="event-detail">
+                        <div class="event-date">
+                            For more information please call:<br />
+                            PABX: +92 (21) 38771999<br />
+                            <br>
+                            OR
+                        </div>
+                        <div class="event-links">
+                            <a href="mailto:admissions@ksbl.edu.pk?subject=KSBL, Contact Us" title="Schedule" class="default-btn">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                      Open House END                              #
+    #----------------------------------------------------------------->
+    <?php
+include('../../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../
+../partials/footer-scripts.php');
+?>
+</body>
+
+</html>
---
+++ mba/financial-assistance/index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<!--  <title> MBA Financial Assistance Support Program | KSBL </title> -->
+<!--  <meta name="description" content=" MBA financial assistance support program and scholarships for deserving and needy students ">
+    <meta name="keywords" content="MBA scholarships, financial assistance, FA, KSBL scholarships, KSBL financial assistance"> -->
+<?php 
+
+include('../../partials/head.php');
+
+?>
+
+<body class="mba financial-assst full-height">
+    <!-----------------------------------------------------------------#
+    #                       Header START                               #
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <!--div class="header-video">
+            <img src="../../images/mba/mba_financialassistance.jpg" alt="MBA"/>
+            </div-->
+        <div class="container">
+           <?php
+include('../partials/navbar.php'); 
+?>
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>Financial Assistance</h1>
+                    <h2>Upholding a learner's right to education for all</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        <div class="header-bottom">
+              <div class="ksbl-programs-wrap">
+                <div class="container">
+                    <div class="ksbl-programs">
+                        <a href="mba/" class="ksbl-program mba active">MBA</a>
+                        <!-- mba -->
+                        <a href="emba/" class="ksbl-program executive-mba">Executive MBA</a>
+                        <!-- executive-mba -->
+                        <a href="ms-business-analytics/" class="ksbl-program engro-leadership-academy ">MS Business Analytics</a>
+                        <a href="meba/" class="ksbl-program executive-edu-mba ">BS MEBA</a>
+                        <a href="bs-its/" class="ksbl-program executive-edu-mba">BS IT&S</a>
+                        <a href="executive-education-services/" class="ksbl-program executive-edu-mba">Executive Education</a>
+                        <!-- executive-edu-mba -->
+                    </div><!-- container -->
+                </div>
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+    #                       Header END                                 #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #                       MBA Department                             #
+    #----------------------------------------------------------------->
+    <div class="departments add">
+        <div class="container">
+            <a href="../admissions/" class="department" title="Admission Process">Admissions Process</a>
+            <a href="../fee/" class="department" title="Fees">Fees</a>
+            <a href="../financial-assistance/" class="department active" title="Financial Assistance">Financial Assistance</a>
+            <a href="../information-sessions/" class="department" title="Open House">Open House</a>
+            <a href="../../why-KSBL.php" class="department" title="Why KSBL">Why KSBL</a>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                   MBA Department END                             #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #              Financial Assistance Start                          #
+    #----------------------------------------------------------------->
+    <div class="mba-section financialassistance assistance">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/mba/financialassistance.jpg" alt="Financial Assistance" />
+            </div>
+            <div class="right-side">
+                <h2>Financial Assistance</h2>
+                <div class="detail">
+                    <p>KSBL endeavours to make business education accessible for everyone, irrespective of socio-economic standing, political views and ethnic background. Our needs blind admissions policy ensures that each application is assessed entirely on merit only. </p>
+                    <p>We make our programs accessible to deserving learners through our Scholarship and Financial Assistance program, which includes the following:
+                        <br>1. Merit Scholarships
+                        <br>2. KSBL Grants
+                        <br>3. Third Party Loans (Qarz-e-Hasana)
+                        <br>4. Income Share Agreements
+                        <br>5. Research Assistantships and Campus Jobs
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                Financial Assistance END                         #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #              Application Assessment Process Start                #
+    #----------------------------------------------------------------->
+    <div class="mba-section applicationassistance assistance">
+        <div class="container">
+            <div class="left-side">
+                <img src="../../images/mba/application-assistance-process.jpg" alt="Application Assessment Process" />
+            </div>
+            <div class="right-side">
+                <h2>Application Assessment Process</h2>
+                <div class="detail">
+                    <p>We have a committee consisting of both management and faculty, which analyzes each case in detail. We determine if the candidate's family has adequate resources to support the program. We base our decision on the financial credentials and other family particulars that the applicants submit. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                      Faculty MBA END                             #
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+    #                      Open House Start                            #
+    #----------------------------------------------------------------->
+    <div class="open-house-section  new-open-houese">
+        <div class="container">
+            <div class="event-program">
+                <!-- <div class="open-house">
+               Apply Now!                   
+            </div> -->
+                <div class="upcoming-event">
+                    <img src="../../images/mba/meet-team.png" alt="Meet the Team">
+                    <h3>Apply Now! </h3>
+                    <div class="event-detail">
+                        <div class="event-date">
+                            Join KSBL - A growth led
+                            learning system that combines
+                            value-driven leadership and top-grade business skills.
+                        </div>
+                        <div class="event-links">
+                            <a href="http://admissions.colaraz.com/signup?institute=ksbledu" target="_blank" title="Apply Now" class="default-btn">Apply</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-program">
+                <!-- <div class="open-house">
+                   Apply Now!                   
+                </div> -->
+                <div class="upcoming-event contact-us">
+                    <h3>More Information</h3>
+                    <div class="event-detail">
+                        <div class="event-date">
+                            For more information please call:<br />
+                            PABX: +92 (21) 38771999<br />
+                            <br>
+                            OR
+                        </div>
+                        <div class="event-links">
+                            <a href="mailto:admissions@ksbl.edu.pk?subject=KSBL, Contact Us" title="Schedule" class="default-btn">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+    #                      Open House END                              #
+    #----------------------------------------------------------------->
+  
+<?php
+include('../../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../
+../partials/footer-scripts.php');
+?>
+</body>
+
+</html>
