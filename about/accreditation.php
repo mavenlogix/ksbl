f33e1f0f8f43a1bc8165305865176085c5a77a72
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 18:50:07 +0500

    updated

---
+++ about/accreditation.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<!-- 	<title>KSBL | About Us, Accreditation</title> -->
+<?php 
+
+include('../partials/head.php');
+
+?>
+
+<body class="mba about">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+                            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.php" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>ACCREDITATION</h1>
+                    <h2>A school that would offer high quality leading edge programs to the many talented young men and women in the country as well as those accepted from abroad</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                       MBA Department                             #
+	#----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="About">About</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.php" class="department" title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../about/accreditation.php" class="department active" title="Accreditation">Accreditation</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+	#                   MBA Department END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      About Curriculum Start                            #
+	#----------------------------------------------------------------->
+    <div class="about-ksbl-section about-curriculum">
+        <div class="container">
+            <div class="left-side">
+                <h2>Chartered by Government of Sindh</h2>
+                The Sindh Provincial Assembly passed KSBL institute bill in December 2012 with a unanimous vote, authorizing the institution to award degrees in business-related disciplines.
+            </div>
+            <div class="right-side">
+                <h2>The Mission</h2>
+                The quest undertaken by KSBL is to bring a value-added and holistic business education degree program. Our academic system is structured to facilitate young talent with a globally-recognized curriculum to reform them into world leaders who can amend conventional ways of leading.
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      About KSBL END                              #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                        Vission Start                            #
+	#----------------------------------------------------------------->
+    <div class="about-ksbl-section the-vissions">
+        <div class="container">
+            <div class="vission-section">
+                <h2>The Vision</h2>
+                In order to continue with our reputation for quality education, the vision of KSBL comprises of these core elements:
+                <div class="vissions d-flex">
+                    <div class="about-vission">
+                        Facilitating learners with <strong>an ultra-modern campus-site</strong> and learning facilities
+                    </div>
+                    <div class="about-vission">
+                        <strong>A renowned faculty</strong> with a strong academic background and equipped with modern-day skills <strong>to deliver education at par with international standards</strong>
+                    </div>
+                    <div class="about-vission">
+                        <strong>Promoting diversity and inclusion</strong> across gender, geography, socio-economic strata, race and ethnic backgrounds
+                    </div>
+                    <div class="about-vission">
+                        <strong>Acquiring international accreditation</strong> and <strong>incorporating international educational standardization</strong> in order to be noted amongst the leading business schools
+                    </div>
+                    <div class="about-vission">
+                        Equipping talented professionals with valuable assets through <strong>modern-day learning</strong> that brings together creativity, leadership potential and a problem-solving attitude
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="../images/about/about-page.jpg" alt="About KSBL" />
+    </div>
+    <!-----------------------------------------------------------------#
+	#                     Vission KSBL END                             #
+	#----------------------------------------------------------------->
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
