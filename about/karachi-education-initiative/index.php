10a4b64a0affe926ca1f0c3a028829210a9d688b
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 18:58:43 +0500

    Update index.php

--- about/karachi-education-initiative/index.php
+++ about/karachi-education-initiative/index.php
@@ -17,7 +17,7 @@ include('../../partials/head.php');
 include('../../partials/navbar.php'); 
 ?>
             <div class="header-content">
-                <a href="../../index.html" title="KSBL" class="logo">
+                <a href="../../index.php" title="KSBL" class="logo">
                     <img src="../../images/ksbl-logo-high.png" width="239" alt="KSBL" />
                 </a>
                 <div class="heder-info">
@@ -38,13 +38,13 @@ include('../../partials/navbar.php');
         <div class="container">
             <a href="../../about/" class="department" title="Vision & Mission">Vision & Mission</a>
             <!-- About -->
-            <a href="../../about/board-of-governors.html" class="department" title="Board of Governors">Board of Governors</a>
+            <a href="../../about/board-of-governors.php" class="department" title="Board of Governors">Board of Governors</a>
             <!-- Board of Governors -->
-            <a href="../../faculty.html" class="department" title="Faculty">Faculty</a>
+            <a href="../../faculty.php" class="department" title="Faculty">Faculty</a>
             <!-- Accreditation -->
             <a href="../../about/karachi-education-initiative/" class="department active" title="KEI">KEI</a>
             <!-- KEi -->
-            <a href="../../contact-us.html" class="department" title="Contact Us">Contact Us</a>
+            <a href="../../contact-us.php" class="department" title="Contact Us">Contact Us</a>
             <!-- Contact -->
         </div><!-- container -->
     </div>
