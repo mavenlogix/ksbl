17c9f4c9be24c721c005a1a813cb2635f0db98b2
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 14:38:04 +0500

    updated

---
+++ executive-education-services/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- <title> Executive Education Programs - Karachi School of Business and Leadership </title> -->
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=" Executive Education at KSBL is a mid-career program for managers and entrepreneurs to prepare for future leadership roles .">
+    <meta name="keywords" content="executive management programs,executive program, evening MBA, management school, business school, post graduate MBA, business education, management sciences">
+     -->
+<?php 
+
+include('../partials/head.php');
+
+?>
+<body class="mba ees">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.html" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>EXECUTIVE EDUCATION</h1>
+                    <h2>First-rate executive training for operational and functional roles for professionals across varied sectors</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        <div class="header-bottom">
+            <div class="ksbl-programs-wrap">
+                <div class="container">
+                    <div class="ksbl-programs">
+                        <a href="../mba/" class="ksbl-program mba">MBA</a>
+                        <!-- mba -->
+                        <a href="../emba/" class="ksbl-program executive-mba">Executive MBA</a>
+                        <!-- executive-mba -->
+                        <a href="../ms-business-analytics/" class="ksbl-program engro-leadership-academy ">MS Business Analytics</a>
+                        <a href="../executive-education-services/" class="ksbl-program executive-edu-mba active">Executive Education</a>
+                        <!-- executive-edu-mba -->
+                        <!--a href="../ela/" class="ksbl-program engro-leadership-academy">Engro Leadership Academy</a-->
+                        <!-- engro-leadership-academy -->
+                    </div><!-- container -->
+                </div>
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                       MBA Department                             #
+	#----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../executive-education-services/" class="department active" title="MBA">About</a>
+            <!-- About Executive Education -->
+            <a href="../executive-education-services/customized-programs/" class="department" title="Custom Programs">Custom Programs</a>
+            <!-- Custom Programs -->
+            <a href="../ela/" class="department" title="Engro Leadership Academy">Engro Leadership Academy</a>
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+	#                   MBA Department END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      About MBA Start                            #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../images/ees/ee-1.jpg" alt="About our MBA" />
+            </div>
+            <div class="right-side">
+                <h2>Changing the Way Executives are Trained</h2>
+                <div class="detail">
+                    <p>Mastery for executives and decision-makers rests in the knowledge they hold, the power to communicate and a detailed yet overarching understanding of the business at hand. Our executive education programs prepare and train for all-embracing components at play.
+                        <p>Executive Education at KSBL encourages and supports intellectual leadership and ongoing innovation across all major disciplines of business education. With the ambition of being the most cutting-edge executive education center in the region, we prepare professionals for the next stage in their career. It is through our deep learning that we understand the expectations of the industry, helping us to tailor our learning interventions, keeping the professionals knowledge and skillset absolutely market-relevant.</p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      About END                              #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      Faculty MBA Start                           #
+	#----------------------------------------------------------------->
+    <div class="mba-section">
+        <div class="container">
+            <div class="left-side">
+                <img src="../images/ees/ee-2.jpg" alt="Faculty" />
+            </div>
+            <div class="right-side">
+                <h2>KSBL Executive Education Program Areas</h2>
+                <div class="detail">
+                    <p>KSBL Open enrollment and customized programs are offered in the following 6 broad areas. You may get in touch with us at execed@ksbl.edu.pk to find out more and discuss your particular personal or organizational need and know about the myriad of learning options as per your needs.
+                        <br><strong>Strategy
+                            <br>Leadership
+                            <br>Personal Development
+                            <br>Finance
+                            <br>Digital
+                            <br>General Management </strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      Faculty MBA END                             #
+	#----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+	#                      Open House Start                            #
+	#----------------------------------------------------------------->
+    <div class="open-house-section  new-open-houese ees-container">
+        <br><br><br>
+        <h3 class="title">Upcoming Programs</h3>
+        <div class="container">
+            <div class="upcoming-programs">
+                <div class="event-detail">
+                    <div class="event-date">
+                        <div class="current-month">
+                            <h3 class="visible-hidden">03</h3>
+                            <h3 class="">
+                                <span>FEB 2021</span>02</h3>
+                            <h3>03</h3>
+                        </div>
+                    </div>
+                    <div class="event-discription ">
+                        <h2>Finance</h2>
+                        <h2>Financial Acumen for Non Financial Executive</h2>
+                        <span>Dr. Tariq Basheer</span>
+                    </div>
+                </div>
+            </div>
+            <div class="upcoming-programs">
+                <div class="event-detail">
+                    <div class="event-date">
+                        <div class="current-month">
+                            <h3 class="">
+                                <span>FEB 2021</span>16</h3>
+                            <h3>17</h3>
+                            <h3>18</h3>
+                        </div>
+                    </div>
+                    <div class="event-discription ">
+                        <h2>Innovation</h2>
+                        <h2>Leading Innovation Strategically</h2>
+                        <span>Dr. Nadeem Javaid</span>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-committee">
+                <span>Since pre-program reading is required, it is advised to send registration(s) at atleast 10 working days prior to the program.</span>
+                <h2>Contact our Executive Education Team</h2>
+                <a href="mailto:execed@ksbl.edu.pk">execed@ksbl.edu.pk</a>
+                <a href="tel:00922134855382">PABX: +923028222016, +92(21) 38771999, Ext 716</a>
+                <a href="../contact-us.html" title="Contact Us">Contact</a>
+            </div>
+        </div>
+    </div>
+    <!-----------------------------------------------------------------#
+	#                      Open House END                              #
+	#----------------------------------------------------------------->
+    <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
