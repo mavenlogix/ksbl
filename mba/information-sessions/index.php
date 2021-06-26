3f75b63df56d5da2e8d53233867b6478551a3f31
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-26 19:09:22 +0500

    updated

---
+++ mba/information-sessions/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<!--   <title> MBA Open House | Karachi School of Business and Leadership </title> -->
+<?php 
+
+include('../../partials/head.php');
+
+?>
+
+
+<body class="mba open-house">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+                <?php
+include('../../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../../index.php" title="KSBL" class="logo">
+                    <img src="../../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+            </div>
+            <div class="open-house-content">
+                <h1>Open House</h1>
+                <div class="page-detail">Become a part of KSBL's Open House and learn more about our programs.
+                    This is an excellent opportunity for prospective students to learn more about our programs and meet officeholders, faculty, alumni, staff and other representatives from your program of interest. You may register now to join in and know more about what makes KSBL unique, be it our leading faculty, action-oriented pedagogy, leadership centralized around ethics and character development, mentorship from eminent leaders, an outstanding placement record and preparing leaders for the new economy.
+                    <br><br>Register now for our Open House here. We look forward to meeting you!
+                    Or, you may schedule a visit. <br>
+                    Contact us at +92 (21) 38771999<br>to schedule a one-on-one meeting with our admissions and career counselor now.
+                </div>
+                <div class="open-house-tabs">
+                    <!-- === Tab 1 === -->
+                    <!-- <div class="tab-section active">
+							<div class="tag-area"><span class="tag orange">UPCOMING</div>
+							<div class="program-details">
+								<div class="about-program">
+									<h2>Meet the Team</h2>
+									<div class="program-date">Wednesday Aug 05, 2020 | 4:30PM - 5:30PM</div>
+									<div class="program-at">Conducting On Zoom</div>
+								</div>
+								<div class="program-connection">
+									<a href="javascript:void(0)" class="btn-highlights">Highlights</a>
+									<a href="../information-sessions-register/" title="Register Now" class="btn-blue">Register Now</a>
+								</div>
+							</div>
+							
+							<div class="highlights-content">
+								<h3>Highlights of the Session</h3>
+								Holistic Overview of MBA Program
+								<br>KMAT Details & Registration Process
+								<br>Guidance from Admission Panel regarding Admission Process
+								<br>Merit-based/Need-based Scholarship
+
+							</div>
+							
+						</div> -->
+                    <!-- === END Tab 1 === -->
+                    <!-- === Tab 2 === -->
+                    <div class="tab-section">
+                        <div class="tag-area"><span class="tag orange">UPCOMING</div>
+                        <div class="program-details">
+                            <div class="about-program">
+                                <h2>Open House</h2>
+                                <div class="program-date"> <a style=" text-decoration: none; color:#FFFFFFB3; " href="https://us02web.zoom.us/meeting/register/tZ0oc-2tqzgoGt3t2ZIMfJcWZ2Sp52GHFRaH ">
+                                        May 29, 2021 (3 pm PST)
+                                    </a> </div>
+                                <div class="program-at"></div>
+                            </div>
+                            <div class="program-connection">
+                                <a href="javascript:void(0)" class="btn-highlights">Highlights</a>
+                                <a href="https://us02web.zoom.us/meeting/register/tZUsf-uhpjwvGdYmP1E4EvINC0nhC0Gwmw-9" title="Register Now" class="btn-blue">Register Now</a>
+                                <!--../information-sessions-register -->
+                            </div>
+                        </div>
+                        <div class="highlights-content">
+                            <h3>Highlights of the Session</h3>
+                            Holistic Overview of Program
+                            <br>KMAT Details & Registration Process
+                            <br>Guidance from Admission Panel regarding Admission Process
+                            <br>Merit-based / Need-based Scholarship
+                        </div>
+                    </div>
+                    <!-- === END Tab 2 === -->
+                    <!-- === Tab 3 === -->
+                    <!-- <div class="tab-section">
+							<div class="tag-area"><span class="tag blue">Conducted</div>
+							<div class="program-details">
+								<div class="about-program">
+									<h2>Meet the Team</h2>
+									<div class="program-date">Wednesday July 29, 2020 | 4:30PM - 5:30PM</div>
+									<div class="program-at">Conducted On Zoom</div>
+								</div>
+								<div class="program-connection">
+									<a href="javascript:void(0)" class="btn-highlights">Highlights</a>
+									<a title="View Recording" class="btn-blue">Recording (coming soon)</a>
+								</div>
+							</div>
+							
+							<div class="highlights-content">
+								<h3>Highlights of the Session</h3>
+								Holistic Overview of MBA Program
+								<br>KMAT Details & Registration Process
+								<br>Guidance from Admission Panel regarding Admission Process
+								<br>Merit-based/Need-based Scholarship
+							</div>
+							
+						</div> -->
+                    <!-- === END Tab 3 === -->
+                    <!-- === Tab 4 === -->
+                    <!-- <div class="tab-section">
+							<div class="tag-area"><span class="tag blue">Conducted</div>
+							<div class="program-details">
+								<div class="about-program">
+									<h2>MBA Open House</h2>
+									<div class="program-date">Wednesday July 29, 2020 | 4:30PM - 5:30PM</div>
+									<div class="program-at">Conducted On Zoom</div>
+								</div>
+								<div class="program-connection">
+									<a href="javascript:void(0)" class="btn-highlights">Highlights</a>
+									<a title="View Recording" class="btn-blue">Recording (coming soon)</a>
+								</div>
+							</div>
+							
+							<div class="highlights-content">
+								<h3>Highlights of the Session</h3>
+								Holistic Overview of MBA Program
+								<br>KMAT Details & Registration Process
+								<br>Guidance from Admission Panel regarding Admission Process
+								<br>Merit-based/Need-based Scholarship
+							</div>
+							</div> -->
+                </div>
+                <!-- === END Tab 4 === -->
+            </div>
+        </div><!-- container -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+      <?php
+include('../../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../../partials/footer-scripts.php');
+?>
+</body>
+
+</html>
