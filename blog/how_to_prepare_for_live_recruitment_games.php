b77755e2246a5ba4dfc51cb2b4c6971200bf6f8c
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-27 15:20:01 +0500

    Update how_to_prepare_for_live_recruitment_games.php

--- blog/how_to_prepare_for_live_recruitment_games.php
+++ blog/how_to_prepare_for_live_recruitment_games.php
@@ -1,163 +1,29 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <?php 
 
 include('../partials/head.php');
 
 ?>
-	
-	<body class="mba open-house center-page category-page">
-	<!-----------------------------------------------------------------#
+
+<body class="mba open-house center-page category-page">
+    <!-----------------------------------------------------------------#
 	#                       Header START                               #
 	#----------------------------------------------------------------->
-		<header id="header" class="run">
-		
-			<div class="container">
-			
-			
-				<div class="header-top">
-				
-					<nav id="top-navigation">
-						<ul>
-							<li><a href="../index.html" title="Home">Home</a></li>
-							<li class="expend">
-								<a href="javascript:void(0)" title="Programs">Programs</a>
-								<ul class="sub-menu">
-									<li><a href="../mba/" title="MBA">MBA</a></li>
-									<li><a href="../emba/" title="Executive MBA">Executive MBA</a></li>
-									<li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li>
-									<li><a href="../executive-education-services/" title="Executive Education">Executive Education</a></li>
-								</ul>
-							</li>
-							<li class="expend">
-								<a href="javascript:void(0)" title="Admissions">Admissions</a>
-								<ul class="sub-menu">
-									<li><a href="../mba/admissions/" title="Admission Process">Admission Process</a></li>
-									<li><a href="../mba/fee/" title="Fee Structure">Fees</a></li>
-									<li><a href="../mba/financial-assistance/" title="Financial Assistance">Financial Assistance</a></li>
-									<li><a href="../mba/information-sessions/" title="Open House Sessions">Open House</a></li>
-									<li><a href="../why-KSBL.html" title="Why KSBL">Why KSBL</a></li>
-								</ul>
-							</li>
-							<li><a href="../blog/category/news-and-updates/" title="News">News</a></li>
-							<li><a href="../alumni/success-stories.html" title="Alumni">Alumni</a></li>
-						</ul>
-					</nav>
-					
-					<div class="menu-serch">
-						<!--  START Search -->
-						<div class="search icon-link">
-							<img src="../images/mba/search-icon-high.png" width="23" height="23" class="search-icon" alt=""/>
-							
-							<div class="search-content header-model">
-								<div class="container">
-									<h2>Search KSBL</h2>
-									<script async src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
-<div class="gcse-search"></div>
-								</div>
-							</div>
-							
-						</div>
-						<!--  END Search -->
-						
-						<!--  START Navigation -->
-						<div class="main-navigation icon-link">
-							<img src="../images/mba/menu-icon-high.png" width="28" height="19" class="menu-icon" alt=""/>
-							
-							<div class="navigation-content header-model">
-								<div class="container">
-									
-									<div class="naviagtions">
-									
-										<a href="../index.html" title="KSBL" class="hmbg-logo">
-											<img src="../ksbl-menu-logo.png" width="239" alt="KSBL"/>
-										</a>
-										
-										<div class="main-menu heder-menu">
-											<ul>
-												<li><a href="../index.html" title="Home">Home</a></li>
-												<li><a href="../blog/category/news-and-updates/" title="News">News</a></li>
-												<li><a href="../alumni/success-stories.html" title="Alumni">Alumni</a></li>												
-												<li><a href="../future-of-work.html"  title="Future of Work">Future of Work</a></li>
-											</ul>
-											
-										</div>
-										
-										<div class="program-services heder-menu">
-											<h3>Programs</h3>
-											<ul class="desktop-menu">
-												<li><a href="../mba/" title="MBA">MBA</a></li>
-												<li><a href="../emba/" title="Executive MBA">Executive MBA</a></li>
-												<li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li>
-												<li><a href="../executive-education-services/" title="Executive Education">Executive Education</a></li>
-											</ul>
-											<ul class="mobile-menu">
-												<li><a href="../mba/" title="MBA">MBA</a></li>
-												<li><a href="../emba/" title="Executive MBA">Executive MBA</a></li>
-												<li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li>
-												<li class="expend">
-													<a href="javascript:void(0)" title="Executive Education">Executive Education</a>
-													<ul class="sub-menu">
-														<li><a href="../executive-education-services/" title="About">About</a></li>
-														<li><a href="../executive-education-services/customized-programs/" title="Custom Programs">Custom Programs</a></li>
-														<li><a href="../ela/" title="Engro Leadership Academy">Engro Leadership Academy</a></li>
-													</ul>
-												</li>
-											</ul>
-											
-										</div>
-
-										<div class="admission heder-menu">
-											<h3>Admissions</h3>
-												<ul>
-													<li><a href="../mba/admissions/" title="Admission Process">Admission Process</a></li>
-													<li><a href="../mba/fee/" title="Fee Structure">Fees</a></li>
-													<li><a href="../mba/financial-assistance/" title="Financial Assistance">Financial Assistance</a></li>
-													<li><a href="../mba/information-sessions/" title="Open House Sessions">Open House</a></li>
-													<li><a href="../why-KSBL.html" title="Why KSBL">Why KSBL</a></li>
-												</ul>
-										</div>
-										
-										
-										
-										<div class="ksbl-menu heder-menu">
-											<h3>About</h3>
-											<ul>
-											
-												<li><a href="../about/"  title="Vision & Mission">Vision & Mission</a></li>
-												<li><a href="../about/board-of-governors.html" title="Board of Governors">Board of Governors</a></li>
-												<li><a href="../faculty.html"  title="Faculty">Faculty</a></li>
-												<li><a href="../about/karachi-education-initiative/"  title="Karachi Education Initiative (KEI)">Karachi Education Initiative (KEI)</a></li>
-												<li><a href="../careers.html"  title="Jobs at KSBL">Jobs</a></li>
-												<li><a href="../contact-us.html"  title="Contact Us">Contact Us</a></li>
-											</ul>
-										</div>
-										
-										
-									</div>
-									
-								</div>
-							</div>
-							
-						</div>
-						<!--  END Navigation -->
-						
-					</div><!-- Search-menu -->
-					
-				</div><!-- Header-top -->
-				
-				<div class="header-content">
-					<a href="../index.html" title="KSBL" class="logo">
-						<img src="../images/ksbl-logo-high.png" width="239" alt="KSBL"/>
-					</a>
-				</div>
-				
-				
-				
-				<div class="open-house-content single-blog">
-					<h1>News & EVENTS . 2020</h1>
-					<div class="page-detail">Stay up to date with the latest happenings at KSBL</div>
-					<!---
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+            </div>
+            <div class="open-house-content single-blog">
+                <h1>News & EVENTS . 2020</h1>
+                <div class="page-detail">Stay up to date with the latest happenings at KSBL</div>
+                <!---
 					
 					<div class="tabs">
 							<a href="#" class="tab active" data-tab="2020">2020</a>
@@ -168,151 +34,50 @@ include('../partials/head.php');
 						</div>
 
 					--->
-					<a href="category/news-and-updates/" class="back-link" title="All News">All News & Events</a>
-					<div class="center-page-content news-and-events container-boxes">
-					
-						<div class="blog-header">
-							<img src="../../../images/blog/live_recruitment_games.jpg" alt="Data Science"  >
-						
-							<div class="blog-name">
-						      <div class="tag">EVENTS</div>
-								<h2>"How to prepare for Live Recruitment Games" by Kamran Kiyani</h2>
-								<div class="post-date">Saturday, February 20th, 2021</div>
-							</div>
-						</div>
-						
-						<div class="blog-detail">
-							<p>At KSBL, we also prepare students for the latest in recruitment practices that are evolving with technology. Since recruitment gaming is a tool that is now popular with many big organizations, a special session was conducted for KSBL students by Mr. Kamran Kiyani on preparatory strategies for live recruitment games.</p>
-
-							
-
-							<p>In the session he shared valuable insights on how it plays a pivotal role in the entire recruitment process along with valuable tips and tricks for students who were new to it.</p>
-
-							
-					  </div>	
-					  
-					  
-					        <!-----------------------------------------------------------------#
+                <a href="category/news-and-updates/" class="back-link" title="All News">All News & Events</a>
+                <div class="center-page-content news-and-events container-boxes">
+                    <div class="blog-header">
+                        <img src="images/blog/live_recruitment_games.jpg" alt="Data Science">
+                        <div class="blog-name">
+                            <div class="tag">EVENTS</div>
+                            <h2>"How to prepare for Live Recruitment Games" by Kamran Kiyani</h2>
+                            <div class="post-date">Saturday, February 20th, 2021</div>
+                        </div>
+                    </div>
+                    <div class="blog-detail">
+                        <p>At KSBL, we also prepare students for the latest in recruitment practices that are evolving with technology. Since recruitment gaming is a tool that is now popular with many big organizations, a special session was conducted for KSBL students by Mr. Kamran Kiyani on preparatory strategies for live recruitment games.</p>
+                        <p>In the session he shared valuable insights on how it plays a pivotal role in the entire recruitment process along with valuable tips and tricks for students who were new to it.</p>
+                    </div>
+                    <!-----------------------------------------------------------------#
 							#                       Other Pictures                             #
 							#----------------------------------------------------------------->
-							<div class="other-pictures">
-						
-									<!--<div class="blog-image-section image">-->
-									<!--	<div class="campus-image">-->
-									<!--		<img src="../images/blog/alumni-reunion-1.jpg" alt="Blog other"/>-->
-									<!--	</div>-->
-									<!--</div>-->
-									
-								
-									
-							</div>
-							<!-----------------------------------------------------------------#
+                    <div class="other-pictures">
+                        <!--<div class="blog-image-section image">-->
+                        <!--	<div class="campus-image">-->
+                        <!--		<img src="images/blog/alumni-reunion-1.jpg" alt="Blog other"/>-->
+                        <!--	</div>-->
+                        <!--</div>-->
+                    </div>
+                    <!-----------------------------------------------------------------#
 							#                      END  Other Pictures                         #
-							#----------------------------------------------------------------->	
-						
-					</div>
-					<a href="category/news-and-updates/" class="back-link" title="Back to News">Back to News & Events</a>
-					
-				</div>
-				
-				
-				
-			</div><!-- container -->
-			
-				
-		</header>
-	<!-----------------------------------------------------------------#
+							#----------------------------------------------------------------->
+                </div>
+                <a href="category/news-and-updates/" class="back-link" title="Back to News">Back to News & Events</a>
+            </div>
+        </div><!-- container -->
+    </header>
+    <!-----------------------------------------------------------------#
 	#                       Header END                                 #
 	#----------------------------------------------------------------->
-	
-	
-		<footer>
-			<div class="container">
-				
-				<div class="footer-left">
-					<div class="left-top">
-						<div class="copy-right">
-							<div class="address">Karachi School of Business & Leadership</div>
-							<div class="copy-right-text">All Rights Reserved. <br>Copyrights &#64; KSBL 2021</div>
-						</div>
-						<div class="main-menu footer-menu">
-							<ul>
-								<li><a href="../index.html" title="Home">Home</a></li>
-								<li><a href="../blog/category/news-and-updates/" title="News">News</a></li>
-								<li><a href="../alumni/success-stories.html" title="Alumni">Alumni</a></li>												
-								<li><a href="../future-of-work.html"  title="Future of Work">Future of Work</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="left-bottom">
-						<div class="social-links">
-							<a href="https://www.facebook.com/KSBLPakistan" target="_blank" title="Facebook" class="facebook"><img src="../images/facebook-icon-high.png" alt="Facebook"/></a>
-							<a href="https://www.linkedin.com/school/ksbl/" target="_blank" title="LinkedIn" class="linkedin"><img src="../images/linkedin-icon-high.png" alt="LinkedIn"/></a>
-							<a href="https://www.youtube.com/user/ksblpakistan" target="_blank" title="YouTube" class="youtube"><img src="../images/youtube-icon-high.png" alt="Youtube"/></a>
-							<a href="https://twitter.com/ksblpakistan" target="_blank" title="Twitter" class="facebook"><img src="../images/twitter-icon-high.png" alt="Twitter"/></a>
-							<a href="https://www.instagram.com/ksbl_pakistan/" target="_blank" title="Instagram" class="facebook"><img src="../images/insta1.png" alt="instagram"/></a>
-						</div>
-						<a href="http://www.shoaworks.com/" target="_blank" class="disiner">Design by ShoaWorks</a>
-					</div>
-				</div>
-				
-				<div class="footer-right">
-				
-					<div class="program-services footer-menu">
-											<h3>Programs</h3>
-											<ul>
-												<li><a href="../mba/" title="MBA">MBA</a></li>
-												<li><a href="../emba/" title="Executive MBA" >Executive MBA</a></li>
-												<li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li>
-												<li><a href="../executive-education-services/" title="Executive Education">Executive Education</a></li>
-												<!--li><a href="ela/" title="Engro Leadership Academy">Engro Leadership Academy</a></li-->
-											</ul>
-											
-											
-										</div>
-										
-										
-										<div class="admissions footer-menu">
-											<h3>Admissions</h3>
-											<ul>
-													<li><a href="../mba/admissions/" title="Admission Process">Admission Process</a></li>
-													<li><a href="../mba/fee/" title="Fee Structure">Fees</a></li>
-													<li><a href="../mba/financial-assistance/" title="Financial Assistance">Financial Assistance</a></li>
-													<li><a href="../mba/information-sessions/" title="Open House Sessions">Open House</a></li>
-													<li><a href="../why-KSBL.html" title="Why KSBL">Why KSBL</a></li>
-												</ul>
-										</div>
+  <?php
+include('../partials/footer.php');
+?>
 
-										<div class="about footer-menu">
-											<h3>About</h3>
-											<ul>
-											
-												<li><a href="../about/"  title="Vision & Mission">Vision & Mission</a></li>
-												<li><a href="../about/board-of-governors.html" title="Board of Governors">Board of Governors</a></li>
-												<li><a href="../faculty.html" title="Faculty">Faculty</a></li>
-												<li><a href="../about/karachi-education-initiative/"  title="Karachi Education Initiative (KEI)">Karachi Education Initiative (KEI)</a></li>
-												<li><a href="../careers.html" title="Jobs at KSBL">Jobs</a></li>
-												<li><a href="../contact-us.html" title="Contact Us">Contact Us</a></li>
-											</ul>
-										</div>
-										
-										<div class="faculty-research footer-menu">
-											<h3>Life at KSBL</h3>
-											<ul>
-												<li><a href="../campus.html" title="Campus">Campus</a></li>
-												<li><a href="../student-placement.html" title="Student Placements">Student Placements</a>
-												<li><a href="../student-achievements.html" title="Student Achievements">Student Achievements</a>
-													<li><a href="../activities-and-clubs.html" title="Societies & Clubs">Societies & Clubs</a>
-											</ul>
-										</div>
-										
-										
-				</div>
-				
-			</div>
-		</footer>
-		<script src="../js/jquery.js"></script>
-		<script src="../js/script.js"></script>
-		<script async  defer  src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
-	</body>
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+    <script async defer src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
+</body>
+
 </html>
