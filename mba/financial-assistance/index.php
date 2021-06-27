10cf1ae9c77880e9b6745c3b870e19e8e6715122
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-27 16:41:39 +0500

    Update index.php

--- mba/financial-assistance/index.php
+++ mba/financial-assistance/index.php
@@ -60,9 +60,9 @@ include('../../partials/navbar.php');
     <div class="departments add">
         <div class="container">
             <a href="mba/admissions/" class="department" title="Admission Process">Admissions Process</a>
-            <a href="fee/" class="department" title="Fees">Fees</a>
-            <a href="financial-assistance/" class="department active" title="Financial Assistance">Financial Assistance</a>
-            <a href="information-sessions/" class="department" title="Open House">Open House</a>
+            <a href="mba/fee/" class="department" title="Fees">Fees</a>
+            <a href="mba/financial-assistance/" class="department active" title="Financial Assistance">Financial Assistance</a>
+            <a href="mba/information-sessions/" class="department" title="Open House">Open House</a>
             <a href="why-KSBL.php" class="department" title="Why KSBL">Why KSBL</a>
         </div>
     </div>
