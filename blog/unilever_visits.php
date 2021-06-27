a37566aa140f541c57538243f4ba56904777022f
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-27 15:37:12 +0500

    updated

--- blog/unilever_visits.php
+++ blog/unilever_visits.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <!-- <title>KSBL | News & Events</title> -->
 <?php 
-$page_meta_key = 'meba';
+
 include('../partials/head.php');
 
 ?>
@@ -18,8 +18,8 @@ include('../partials/navbar.php');
 ?>
 
             <div class="header-content">
-                <a href="../index.html" title="KSBL" class="logo">
-                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
                 </a>
             </div>
             <div class="open-house-content single-blog">
@@ -39,7 +39,7 @@ include('../partials/navbar.php');
                 <a href="category/news-and-updates/" class="back-link" title="All News">All News & Events</a>
                 <div class="center-page-content news-and-events container-boxes">
                     <div class="blog-header">
-                        <img src="../images/blog/unilever_visits.jpg" alt="Unilever visits KSBL" />
+                        <img src="images/blog/unilever_visits.jpg" alt="Unilever visits KSBL" />
                         <div class="blog-name">
                             <div class="tag">EVENTS</div>
                             <h2>Unilever Visits KSBL to conduct Campus Drive at KSBL</h2>
@@ -58,22 +58,22 @@ include('../partials/navbar.php');
                     <div class="other-pictures">
                         <div class="blog-image-section image">
                             <div class="campus-image">
-                                <img src="../images/blog/unilever_visits-1.jpg" alt="Blog other" />
+                                <img src="images/blog/unilever_visits-1.jpg" alt="Blog other" />
                             </div>
                         </div>
                         <div class="blog-image-section image">
                             <div class="campus-image">
-                                <img src="../images/blog/unilever_visits-2.jpg" alt="Blog other 2" />
+                                <img src="images/blog/unilever_visits-2.jpg" alt="Blog other 2" />
                             </div>
                         </div>
                         <div class="blog-image-section image">
                             <div class="campus-image">
-                                <img src="../images/blog/unilever_visits-3.jpg" alt="Blog other 3" />
+                                <img src="images/blog/unilever_visits-3.jpg" alt="Blog other 3" />
                             </div>
                         </div>
                         <div class="blog-image-section image">
                             <div class="campus-image">
-                                <img src="../images/blog/unilever_visits-4.jpg" alt="Blog other 4" />
+                                <img src="images/blog/unilever_visits-4.jpg" alt="Blog other 4" />
                             </div>
                         </div>
                     </div>
@@ -88,80 +88,15 @@ include('../partials/navbar.php');
     <!-----------------------------------------------------------------#
 	#                       Header END                                 #
 	#----------------------------------------------------------------->
-    <footer>
-        <div class="container">
-            <div class="footer-left">
-                <div class="left-top">
-                    <div class="copy-right">
-                        <div class="address">Karachi School of Business & Leadership</div>
-                        <div class="copy-right-text">All Rights Reserved. <br>Copyrights &#64; KSBL 2021</div>
-                    </div>
-                    <div class="main-menu footer-menu">
-                        <ul>
-                            <li><a href="../index.html" title="Home">Home</a></li>
-                            <li><a href="../blog/category/news-and-updates/" title="News">News</a></li>
-                            <li><a href="../alumni/success-stories.html" title="Alumni">Alumni</a></li>
-                            <li><a href="../future-of-work.html" title="Future of Work">Future of Work</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="left-bottom">
-                    <div class="social-links">
-                        <a href="https://www.facebook.com/KSBLPakistan" target="_blank" title="Facebook" class="facebook"><img src="../images/facebook-icon-high.png" alt="Facebook" /></a>
-                        <a href="https://www.linkedin.com/school/ksbl/" target="_blank" title="LinkedIn" class="linkedin"><img src="../images/linkedin-icon-high.png" alt="LinkedIn" /></a>
-                        <a href="https://www.youtube.com/user/ksblpakistan" target="_blank" title="YouTube" class="youtube"><img src="../images/youtube-icon-high.png" alt="Youtube" /></a>
-                        <a href="https://twitter.com/ksblpakistan" target="_blank" title="Twitter" class="facebook"><img src="../images/twitter-icon-high.png" alt="Twitter" /></a>
-                        <a href="https://www.instagram.com/ksbl_pakistan/" target="_blank" title="Instagram" class="facebook"><img src="../images/insta1.png" alt="instagram" /></a>
-                    </div>
-                    <a href="http://www.shoaworks.com/" target="_blank" class="disiner">Design by ShoaWorks</a>
-                </div>
-            </div>
-            <div class="footer-right">
-                <div class="program-services footer-menu">
-                    <h3>Programs</h3>
-                    <ul>
-                        <li><a href="../mba/" title="MBA">MBA</a></li>
-                        <li><a href="../emba/" title="Executive MBA">Executive MBA</a></li>
-                        <li><a href="../ms-business-analytics/" title="MS Business Analytics">MS Business Analytics</a></li>
-                        <li><a href="../executive-education-services/" title="Executive Education">Executive Education</a></li>
-                        <!--li><a href="ela/" title="Engro Leadership Academy">Engro Leadership Academy</a></li-->
-                    </ul>
-                </div>
-                <div class="admissions footer-menu">
-                    <h3>Admissions</h3>
-                    <ul>
-                        <li><a href="../mba/admissions/" title="Admission Process">Admission Process</a></li>
-                        <li><a href="../mba/fee/" title="Fee Structure">Fees</a></li>
-                        <li><a href="../mba/financial-assistance/" title="Financial Assistance">Financial Assistance</a></li>
-                        <li><a href="../mba/information-sessions/" title="Open House Sessions">Open House</a></li>
-                        <li><a href="../why-KSBL.html" title="Why KSBL">Why KSBL</a></li>
-                    </ul>
-                </div>
-                <div class="about footer-menu">
-                    <h3>About</h3>
-                    <ul>
-                        <li><a href="../about/" title="Vision & Mission">Vision & Mission</a></li>
-                        <li><a href="../about/board-of-governors.html" title="Board of Governors">Board of Governors</a></li>
-                        <li><a href="../faculty.html" title="Faculty">Faculty</a></li>
-                        <li><a href="../about/karachi-education-initiative/" title="Karachi Education Initiative (KEI)">Karachi Education Initiative (KEI)</a></li>
-                        <li><a href="../careers.html" title="Jobs at KSBL">Jobs</a></li>
-                        <li><a href="../contact-us.html" title="Contact Us">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div class="faculty-research footer-menu">
-                    <h3>Life at KSBL</h3>
-                    <ul>
-                        <li><a href="../campus.html" title="Campus">Campus</a></li>
-                        <li><a href="../student-placement.html" title="Student Placements">Student Placements</a>
-                        <li><a href="../student-achievements.html" title="Student Achievements">Student Achievements</a>
-                        <li><a href="../activities-and-clubs.html" title="Societies & Clubs">Societies & Clubs</a>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="../js/jquery.js"></script>
-    <script src="../js/script.js"></script>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
     <script async src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
 </body>
 
