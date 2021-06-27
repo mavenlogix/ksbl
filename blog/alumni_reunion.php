6cf53d1dcce7d38576fc9a2785cdf3665625f7a1
ahtesham127 <ahtesham_hussain@outlook.com>
2021-06-27 14:40:07 +0500

    updated

---
+++ blog/alumni_reunion.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- <title>KSBL | News & Events</title> -->
+<?php 
+$page_meta_key = 'meba';
+include('../partials/head.php');
+
+?>
+
+<body class="mba open-house center-page category-page">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.html" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+            </div>
+            <div class="open-house-content single-blog">
+                <h1>News & EVENTS . 2020</h1>
+                <div class="page-detail">Stay up to date with the latest happenings at KSBL</div>
+                <!---
+					
+					<div class="tabs">
+							<a href="#" class="tab active" data-tab="2020">2020</a>
+							<a href="#" class="tab" data-tab="2019">2019</a>
+							<a href="#" class="tab" data-tab="2018">2018</a>
+							<a href="#" class="tab" data-tab="archive">Archive</a>
+							
+						</div>
+
+					--->
+                <a href="category/news-and-updates/" class="back-link" title="All News">All News & Events</a>
+                <div class="center-page-content news-and-events container-boxes">
+                    <div class="blog-header">
+                        <img src="../images/blog/alumni-reunion.jpg" alt="Signs an MoU" />
+                        <div class="blog-name">
+                            <div class="tag">EVENTS</div>
+                            <h2>KSBL Organized Alumni Reunion 2020</h2>
+                            <div class="post-date">Monday, February 17th, 2020</div>
+                        </div>
+                    </div>
+                    <div class="blog-detail">
+                        <p>The KSBL Alumni from batches of 2014 to 2019 were given a warm welcome at the Grand Alumni Reunion 2020, by the Faculty, management and current students. The evening was filled with recalling memories, sharing new initiatives and celebrating accomplishments of the Institute, Alumni and current students. Loads of Laughter, excitement and new memories were created, to be cherished in the coming years.</p>
+                        <p>At the beginning of session, Dr. Zeeshan Ahmed (Dean, KSBL) welcomed graduates and shared latest initiatives that have been launched under the umbrella of KSBL. He highlighted the establishment and success of Urban Youth Project (UYP) and other strengthening partnerships. He announced that in upcoming months KSBL will soon become an entrepreneurial centre with new initiatives in the pipeline. In addition to that he also mentioned new members on the KSBL board, like Ms. Seema Kamil (CEO, UBL) and Ms. Tania Aidrus (Head of Digital Economy, Pakistan), to name a few. The Dean invited the high achiever Alumni along with current students who won prestigious competitions recently and in the past, to celebrate and recognize their hard work, specifically the CFA competitions winning streak.</p>
+                        <p>Later Dr. Jawaid Abdul Ghani (Professor of Strategy & Market Research) and Dr. Muhammad Athar Siddiqui (Associate Dean, KSBL) recalled their class memories with Alumni and congratulated them for their achievements, in professional life.</p>
+                        <p>The Alumni also invited faculty on stage for a light-hearted conversation to reminisce their old memories with their mentors. The activities were followed by elections conducted for the Alumni Council, where all nominated prospects delivered their elevator pitches.</p>
+                        <p>The event concluded with exciting photo sessions of all batches, and dinner at which alumni reconnected and caught up with happenings in each other's lives.</p>
+                    </div>
+                    <!-----------------------------------------------------------------#
+							#                       Other Pictures                             #
+							#----------------------------------------------------------------->
+                    <div class="other-pictures">
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-1.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-2.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-3.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-4.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-5.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-6.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-7.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-8.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-9.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-10.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-11.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-12.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-13.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-14.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-15.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-16.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-17.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-18.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-19.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-20.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-21.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-22.jpg" alt="Blog other 4" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-23.jpg" alt="Blog other" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-24.jpg" alt="Blog other 2" />
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="../images/blog/alumni-reunion-25.jpg" alt="Blog other 3" />
+                            </div>
+                        </div>
+                    </div>
+                    <!-----------------------------------------------------------------#
+							#                      END  Other Pictures                         #
+							#----------------------------------------------------------------->
+                </div>
+                <a href="category/news-and-updates/" class="back-link" title="Back to News">Back to News & Events</a>
+            </div>
+        </div><!-- container -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+    <script async defer src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
+</body>
+
+</html>
