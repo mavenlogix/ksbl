c7e4577d9fa85124b8bb94cde5de073d896e45c5
ahtesham127 <ahtesham_hussain@outlook.com>
2021-07-02 21:32:59 +0500

    updated

--- why-KSBL.php
+++ why-KSBL.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <?php 
 $page_meta_key = 'why-KSBL';
 include('./partials/head.php');
@@ -37,49 +36,65 @@ include('./partials/navbar.php');
                     <p>Five distinctive overarching competencies make the foundation of any academic learning <br> at KSBL leaving a transformative lifelong mark</p>
                 </div><!-- container -->
             </div>
-            </div><!-- container -->
+        </div><!-- container -->
         <div class="whyksbl_accordion">
-            <div id="main">
-  <div class="container">
-<div class="accordion" id="faq">
-                    <div class="card">
-                        <div class="card-header" id="faqhead1">
-                            <a href="#" class="btn btn-header-link" data-toggle="collapse" data-target="#faq1"
-                            aria-expanded="true" aria-controls="faq1"><span>1</span>Ethical Leadership Behavior</a>
+            <div class="whyksbl_accordion_child">
+                <div class="container">
+                    <div class="accordion" id="faq">
+                        <div class="card">
+                            <div class="card-header" id="faqhead1">
+                                <a href="#" class="btn btn-header-link" data-toggle="collapse" data-target="#faq1" aria-expanded="true" aria-controls="faq1"><span>1</span>Ethical Leadership Behavior</a>
+                            </div>
+                            <div id="faq1" class="collapse show" aria-labelledby="faqhead1" data-parent="#faq">
+                                <div class="card-body">
+                                    Ethical leadership is at the very foundation of learning at KSBL. Our students are systematically encouraged to reflect on their values and beliefs and impact of their decisions. Our ‘people’ and ‘processes’ exhibit ethical conduct, while the curriculum is designed to provide opportunities for both self-development, interpersonal development, and community development. 
+                                </div>
+                            </div>
                         </div>
-
-                        <div id="faq1" class="collapse show" aria-labelledby="faqhead1" data-parent="#faq">
-                            <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                       
+                        <div class="card">
+                            <div class="card-header" id="faqhead2">
+                                <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq2" aria-expanded="true" aria-controls="faq2"><span>2</span>Entrepreneurialism</a>
+                            </div>
+                            <div id="faq2" class="collapse " aria-labelledby="faqhead2" data-parent="#faq">
+                                <div class="card-body">
+                                   We provide students an intellectual ecosystem that encourages creativity and problem-solving, so they cultivate an entrepreneurial mindset. The mindset of seeking new opportunities that create value, and effectively managing resources is embedded into every aspect of our teaching. This training on outcome-based decision making and facing challenges is an integral part of any learning at KSBL. 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="faqhead3">
+                                <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq3" aria-expanded="true" aria-controls="faq3"><span>3</span>Technology Savviness</a>
+                            </div>
+                            <div id="faq3" class="collapse " aria-labelledby="faqhead3" data-parent="#faq">
+                                <div class="card-body">
+                                   Digital literacy is crucial to institutional survival in today’s world. We thoroughly prepare all our students for a digital future that is both innovative and inclusive and empowers them for better decision making. Teaching how technology is evolving and what happens behind the scenes helps students become more analytical, and creative in their approach to using it for personal and professional gains. 
+                                </div>
+                            </div>
+                        </div>
+                         <div class="card">
+                            <div class="card-header" id="faqhead4">
+                                <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq4" aria-expanded="true" aria-controls="faq4"><span>4</span>Global Mindedness</a>
+                            </div>
+                            <div id="faq4" class="collapse " aria-labelledby="faqhead3" data-parent="#faq">
+                                <div class="card-body">
+                                    Global citizenship skills are the acknowledgement that we all feed into one interconnected system driven by technology, financial systems, and politics. A core component of our curriculum is creating global competencies and building awareness about the wider world. Knowledge of how we fit into the global community creates self-assurance to thrive in a volatile interconnected world.  
+                                </div>
+                            </div>
+                        </div>
+                          <div class="card">
+                            <div class="card-header" id="faqhead5">
+                                <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq5" aria-expanded="true" aria-controls="faq5"><span>5</span>Sustainability Focus</a>
+                            </div>
+                            <div id="faq5" class="collapse " aria-labelledby="faqhead4" data-parent="#faq">
+                                <div class="card-body">
+                                    With a growing global population and finite resources of our shared planet, there is an increasing need to adopt environmentally and socially responsible business practices. KSBL programs broaden knowledge of the complex aspects of sustainability issues and develop meaningful insights.  These changing global paradigms also make it a priority for us to be part of a network of like-minded stakeholders supporting each other on the sustainability agenda.
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="card">
-                        <div class="card-header" id="faqhead2">
-                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq2"
-                            aria-expanded="true" aria-controls="faq2">2</a>
-                        </div>
-
-                        <div id="faq2" class="collapse" aria-labelledby="faqhead2" data-parent="#faq">
-                            <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </div>
-                        </div>
-                    </div>
-                   
                 </div>
-    </div>
-  </div>
+            </div>
         </div>
     </header>
     <!-----------------------------------------------------------------#
@@ -89,14 +104,27 @@ include('./partials/navbar.php');
     #                  Immersive Learning Start                        #
     #----------------------------------------------------------------->
     <div class="why-ksbl-section immersive-learning">
+        <div class="container d-block">
+           
+            <div class="text-center">
+                <h2>We combine the technical with the strategic. Our curriculum is designed in view of both local and global considerations. With this at its core, the curriculum inspires creativity and reflection, where students are encouraged to demonstrate all these attributes while they are with us. 
+This combination is grounded in social consciousness and emphasis on the principles of ethics.</h2> 
+<h2>
+At KSBL, students do not just receive an education – we create a profound impact on their character instilling traits like empathy and humility.  This holistic approach is all learning at KSBL helps our students equally thrive in large corporations, start-ups, or the gig-economy.</h2>
+             
+            </div>
+        </div>
+    </div>
+       <div class="why-ksbl-section research-learning">
         <div class="container">
             <div class="left-side">
-                <img src="images/immersive-learning.jpg" alt="Immersive Learning" />
+                <img src="images/research-based-learning.jpg" alt="Research based Learning" />
             </div>
             <div class="right-side">
-                <h2>Learning Continues, Beyond the Classroom</h2>
+                <h2>Pedagogy: Learning that is Research-Based & Growth-Led
+                </h2>
                 <div class="detail">
-                    <p>Our educational philosophy emphasizes on developing systematic thinking, continual curiosity to improve and industry dynamism by bringing in subject-matter experts who challenge students to experience real problems beyond the trajectories of theory. At KSBL, this happens in a premeditated framework which ensures that ethical behaviour and character development also do not go amiss. Our curriculum is complemented by a value-driven and industry-based approach to foster ethical decision-making and knowledge & humility.</p>
+                    <p><b>Our pedagogical framework rests on taking onboard academic professionals and business experts with a real-world approach to design & deliver the curriculum in conjunction with industrial demands. While developing a leadership hub at KSBL, we wish to equip individuals with both the right character and the perfect capabilities to make a mark in the business world.</p>
                 </div>
             </div>
         </div>
@@ -134,29 +162,15 @@ include('./partials/navbar.php');
     <!-----------------------------------------------------------------#
     #                   Research Learning Start                        #
     #----------------------------------------------------------------->
-    <div class="why-ksbl-section research-learning">
-        <div class="container">
-            <div class="left-side">
-                <img src="images/research-based-learning.jpg" alt="Research based Learning" />
-            </div>
-            <div class="right-side">
-                <h2>Pedagogy: Learning that is Research-Based & Growth-Led
-                </h2>
-                <div class="detail">
-                    <p><b>Our pedagogical framework rests on taking onboard academic professionals and business experts with a real-world approach to design & deliver the curriculum in conjunction with industrial demands. While developing a leadership hub at KSBL, we wish to equip individuals with both the right character and the perfect capabilities to make a mark in the business world.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+ 
     <!-----------------------------------------------------------------#
     #                    Research Learning END                         #
     #----------------------------------------------------------------->
-   <?php
+    <?php
 include('./partials/footer.php');
 ?>
-
-<!-- footer end -->
-<?php
+    <!-- footer end -->
+    <?php
 include('./partials/footer-scripts.php');
 ?>
 </body>
