50edab1c186dac4b1e1aab3b5e541982eaf15b6d
sajidkhattak1996 <Dev2.maven@gmail.com>
2021-07-03 12:38:30 +0500

    Add the profile page of Dr Asim khwaja

---
+++ faculty-profile/Dr-Asim-Khwaja.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+   <?php 
+$page_meta_key = 'faculty';
+include('../partials/head.php');
+
+?>
+
+<body class="mba aboutt bog main-faculty">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+             <?php
+                include('../partials/navbar.php'); 
+            ?>
+
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="faculty_grid">
+                <div class="faculty_grid_left dr-asim-khwaja">
+                    <div class="faculty_grid_head">
+                        <h1>Dr. Asim Khwaja</h1>
+                        <h4>Assistant Professor, Decision Sciences</h4>
+                        <a href="mail:to">asim.khuwaja@ksbl.edu.pk</a>
+                    </div>
+                </div>
+                <div class="faculty_grid_center">
+                    <ul class="nav nav-tabs border-0" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#vita">Profile Summary</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#credentials">Credentials</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#interests">Consulting & Teaching</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#publications">Publications & Research</a>
+                        </li>
+                    </ul>
+                    <!-- Tab panes -->
+                    <div class="tab-content mt-2">
+                        <div id="vita" class=" tab-pane active">
+                            <p>With a bachelors in electrical engineering (Pakistan), masters in computer science (USA) and a PhD in information sciences & engineering (Australia), Dr Asim Khwaja brings forward the experience of researcher, educator and industry professional - across three continents, and over two decades - USA, Pakistan, and Australia; near 15 years of which, in the facilitation and delivery of classroom instruction and training and 5+ years in the execution of software delivery at Intel (US), and Verizon Data Services (USA).</p>
+                            <p>Dr Khwaja's research interests are devoted to the intersection of computer science and human biology - the nexus of machine learning, computer vision, and image processing on one hand, and neuroscience, psychology, and personal development on the other.</p>
+                            <p>Dr Khwaja is a dedicated educationist with interest in all the educational levels from K-12 to all the way up. He has experimented with different learning strategies and have spoken on different issues related to early education. He with the help of his wife has home-schooled all his kids and strongly believes in bringing a change in current educational system.</p>
+                            <p>Dr Khwaja has also contributed to social and community work among diverse groups of people in Pakistan, USA, and Australia in the past 18 years. He has delivered lectures worldwide on topics of religion, science, psychology, sociology, and ethics.</p>
+                        </div>
+                        <div id="credentials" class=" tab-pane fade">
+                            <h4 class="text-primary">Education</h4>
+                            <p>
+                                Ph.D., Information Sciences & Engineering (Computer Vision)<br>
+                                2012<br>
+                                The Australian National University, Canberra, Australia<br>
+                                Thesis: Exploring the Visual System and its Applications in Image Reconstruction, Contrast Enhancement, and<br>
+                                Object Recognition
+                            </p>
+                            <p>
+                                MS, Computer Science (Computer Graphics, CAGD, & Software Engineering)<br>
+                                1999<br>
+                                Arizona State University, Tempe, Arizona, USA<br>
+                                Graduated with a cumulative GPA of 4.0/4.0<br>
+                                Thesis: Inverse Kinematics of Arbitrary Robotic Manipulators using Genetic Algorithms
+                            </p>
+                            <p>
+                                BE, Electrical Engineering<br>
+                                1994<br>
+                                N.E.D. University of Engineering and Technology, Karachi, Pakistan<br>
+                                Final Project: Optical Character Recognition using Artificial Neural Networks
+                            </p>
+                            <p><b>Professional Trainings</b></p>
+                            <p>
+                                First-Aid Training, St John Ambulance, Australia<br>
+                                2013
+                            <p>
+                            <p>
+                                Communicating Science to Peers Workshop, ANU College of Science, Australia<br>
+                              2008
+                            <p>
+                            <p>
+                                Time Management Workshop, Verizon Data Services, USA<br>
+                                1999
+                            <p>
+                            <p>
+                                Sleep Management Workshop, TimeLenders, Pakistan<br>
+                                2015
+                            <p>
+                            <p>
+                                Strategic Time Management Workshop, TimeLenders, Pakistan<br>
+                                2016<br>
+                                Strategic Vision Workshop, TimeLenders, Pakistan
+                            <p>
+                            
+                        </div>
+                        <div id="interests" class=" tab-pane fade">
+                            <p>
+                            DHA Suffa University, Karachi, Pakistan
+                            Dec 2019 – Current
+                            Assistant Professor
+                            </p>
+
+                            <p>
+                            National University of Sciences & Technology (NUST), Karachi, Pakistan
+                            Apr 2014 – Dec 2019
+                            Assistant Professor
+                            </p>
+
+                            <p>
+                            University of Canberra, Canberra, Australia
+                            Jan 2009 – Dec 2013
+                            Lecturer / Research Assistant
+                            </p>
+
+                            <p>
+                            NICTA, Canberra Research Labs, Canberra, Australia
+                            Jun 2011 – Aug 2011
+                            Research Engineer
+                            </p>
+
+                            <p>
+                            The Australian National University, Canberra, Australia
+                            2007 – 2010
+                            PhD Candidate / Software Developer / Teaching Assistant
+                            </p>
+
+                            <p>
+                            NICTA, Canberra Research Labs, Canberra, Australia
+                            2007
+                            Web Developer / Assistant Conference Organizer
+                            </p>
+
+                            <p>
+                            National University of Sciences & Technology (NUST), Karachi, Pakistan
+                            2004 – 2007
+                            Lecturer / Hon. Asst. Professor
+                            </p>
+
+                            <p>
+                            Intel Corporation, Chandler, Arizona, USA
+                            2000 – 2003
+                            Systems Validation Engineer / Software Engineer
+                            </p>
+
+                            <p>
+                            Verizon Data Services, Dallas, Texas, USA
+                            1999 – 2000
+                            Software Systems Architect
+                            </p>
+
+                            <p>
+                            Arizona State University, Tempe, Arizona, USA
+                            1996 – 1998
+                            Teaching Assistant / Software Developer
+                            </p>
+                            <p><b>Workshops Conducted:</b></p>
+
+                            <p>
+                            Sleep Science (Personal Development)
+                            PNEC, NUST
+                            </p>
+
+                            <p>
+                            The Power of Belief (Personal Development, Psychology)
+                            PNEC, NUST  
+                            </p>
+
+                            <p>
+                            Is Seeing Believing + The Invisible Gorilla (Personal Development, Psychology)
+                            PNEC, NUST
+                            </p>
+
+                            <p>
+                            MATLAB Basics (Engineering, Computer Science)
+                            PNEC, NUST
+                            </p>
+
+                            <p>
+                            Introduction to Web Development (Computer Science)
+                            PNEC, NUST
+                            </p>
+
+                            <p>
+                            <b>Information Sessions Conducted & TV Appearances:</b>
+                            </p>
+
+                            <p>
+                            METRO TV Education Guide Program on DHA Suffa University, 2020 
+                            Link: https://www.youtube.com/watch?v=ooh6PGrqoD0#t=9m40s
+                            (Click on the link to view it on YouTube)
+                            </p>
+
+                            <p>
+                            DSU 1st Virtual Info Session, May 2020
+                            https://www.youtube.com/watch?v=NBCFCWVlEe4#t=8m42s
+                            (Click on the link to view it on YouTube)
+                            </p>
+
+                            <p>
+                            DSU 2nd Virtual Info Session
+                            https://www.youtube.com/watch?v=1UIzu7aV1V4#t=14m34s
+                            (Click on the link to view it on YouTube)
+                            </p>
+
+                        </div>
+                        <div id="publications" class=" tab-pane fade"><br>
+                           <p>
+                           A. Khwaja, A. Asthana and R. Goecke. Illumination and Expression Invariant Face Recognition Using SSIM Based Sparse Representation. Proceedings of the 2010 IEEE International Conference on Pattern Recognition ICPR2010, Istanbul, Turkey, 23-26 Aug. 2010. IEEE.
+                           </p>
+
+                           <p>
+                           A. Khwaja and R. Goecke. Biologically Inspired Contrast Enhancement Using Asymmetric Gain Control. Proceedings of the International Conference on Digital Image Computing: Techniques and Applications DICTA2009, Melbourne, Australia, 1-3 Dec. 2009. APRS.
+                           </p>
+
+                           <p>
+                           A. Asthana, A. Khwaja and R. Goecke. Automatic Frontal Face Annotation and AAM Building for Arbitrary Expressions from a Single Frontal Image Only. Proceedings of the 2009 IEEE International Conference on Image Processing ICIP2009, Cairo, Egypt, 7-11 Nov. 2009. IEEE.
+                           </p>
+
+                           <p>
+                           A. Khwaja and R. Goecke. Image Reconstruction from Contrast Information. In Digital Image Computing: Techniques and Applications (DICTA 2008), Canberra, Australia, 1-3 December 2008. APRS.
+                           </p>
+
+                           <p>
+                           A. Khwaja, M.O. Rahman, M.G. Wagner. Inverse Kinematics of Arbitrary Robotic Manipulators using Genetic Algorithms. 6th International Symposium on Advances in Robot Kinematics, Austria, June 29 – July 4, 1998.
+                           </p>
+
+                           <p>
+                           A. Khwaja. Handling Chaos with Neural Networks. Presented in the 1993 student seminar of the Institute of Electrical and Electronics Engineers of Pakistan (IEEEP), Karachi, Pakistan, 1993.
+                           </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                    <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+
+                       <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq E Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br> Accounting & Finance<br>
+                                    Head of Department, Business Administration</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Jawaid-Abdul-Ghani.php">Dr. Jawaid Abdul Ghani
+                                <span>Professor<br>Strategy & Marketing Research </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waheed-Ali-Umrani.php">Dr. Waheed Ali Umrani
+                                <span>Associate Professor <br>HR & OB</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shoaib-ul-Haq.php">Dr. Shoaib ul Haq
+                                <span>Associate Professor<br>General Management & Digital Economy</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Talha-Salam.php">Dr. Muhammad Talha Salam
+                                <span>Assistant Professor<br>Marketing & Analytics</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+        include('../partials/footer.php');
+    ?>
+    <!-- footer end -->
+   <?php
+        include('../partials/footer-scripts.php');
+    ?>
+</body>
+
+</html>
