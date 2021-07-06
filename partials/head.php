f8c8269293202b3da99b1346781dc582dc875b09
ahtesham127 <ahtesham_hussain@outlook.com>
2021-07-06 22:04:10 +0500

    Updated hover css

--- partials/head.php
+++ partials/head.php
@@ -34,6 +34,7 @@ define('SCRIPT_BASE', str_replace(['http://','https://'], '', $url));
         <meta property="<?= $meta_key ?>" name="<?= $meta_key ?>" content="<?= $meta_value ?>">
     <?php endforeach ?>
     <link rel="icon" href="favicon.png" sizes="16x16">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style-mba.css">
     <link rel="stylesheet" href="./css/style.css">
