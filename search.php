eafe0e8ea039eee34747c1d40194a86e2eb92a24
ahtesham127 <ahtesham_hussain@outlook.com>
2021-07-09 21:25:49 +0500

    updated

--- search.php
+++ search.php
@@ -21,7 +21,7 @@ gtag('config', 'UA-55371745-1');
     <!-----------------------------------------------------------------#
 	#                       Header START                               #
 	#----------------------------------------------------------------->
-    <header id="header" class="run">
+    <header id="header " class="run search_page">
         <div class="container">
             <?php
 include('./partials/navbar.php'); 
@@ -33,20 +33,33 @@ include('./partials/navbar.php');
             </div>
             <div class="open-house-content">
                 <h1>SEARCH RESULTS</h1>
-                <div class="page-detail">We found 11 results matching your search on XYZ subject</div>
+                <div class="page-detail">Your Searched For <b>'MBA FEE'</b> - 5 results found:</div>
                 <div class="open-house-tabs">
                     <!-- === Tab 1 === -->
                     <div class="tab-section active">
                         <div class="content-section-search">
                             <div class="address-area">
-                                <h2>General Enquiries</h2>
+                                <h2>MBA Fee Structure</h2>
                                 <div class="complete-address">
+                                    <a href="" class="font-weight-bold">www.ksbl.edu.pk > mba > Fee</a>
                                     <p>KSBL Curriculum . Core Competencies</p>
                                 </div>
                             </div>
                           
                         </div>
+                         <div class="content-section-search">
+                            <div class="address-area">
+                                <h2>General Enquiries</h2>
+                                <div class="complete-address">
+                                    <a href="" class="font-weight-bold">www.ksbl.edu.pk > mba > Fee</a>
+                                    <p>KSBL Curriculum . Core Competencies</p>
+                                </div>
+                            </div>
+                          
+                        </div>
+
                     </div>
+
                     <!-- === END Tab 1 === -->
                 </div>
             </div>
