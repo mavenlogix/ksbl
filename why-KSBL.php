28c296dc87ff9c255783f3b7971f03efabb6f7e9
ahtesham127 <ahtesham_hussain@outlook.com>
2021-07-13 20:48:51 +0500

    Fixed logo

--- why-KSBL.php
+++ why-KSBL.php
@@ -27,7 +27,7 @@ include('./partials/head.php');
 include('./partials/navbar.php'); 
 ?>
             <div class="header-content d-block">
-                <a href="index.php" title="KSBL" class="logo left-margin">
+                <a href="index.php" title="KSBL" class="logo d-block">
                     <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
                 </a>
                 <div class="header_center text-center">
