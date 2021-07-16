e1b76d479cd4304a5a50675e50c4b00f4a5597a3
ahtesham127 <ahtesham_hussain@outlook.com>
2021-07-16 21:25:38 +0500

    Update why-KSBL.php

--- why-KSBL.php
+++ why-KSBL.php
@@ -27,7 +27,7 @@ include('./partials/head.php');
 include('./partials/navbar.php'); 
 ?>
             <div class="header-content d-block">
-                <a href="index.php" title="KSBL" class="logo d-block">
+                <a href="index.php" title="KSBL" class="logo d-block d-md-inline-block">
                     <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
                 </a>
                 <div class="header_center text-center">
