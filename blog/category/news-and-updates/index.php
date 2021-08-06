f70e9bdcb881d747eb881f28ce90b0b3be42d8cf
sajidkhattak1996 <Dev2.maven@gmail.com>
2021-08-06 11:31:09 +0500

    2nd news added to the main blog page

--- blog/category/news-and-updates/index.php
+++ blog/category/news-and-updates/index.php
@@ -37,7 +37,20 @@ include('../../../partials/navbar.php');
 
 					--->
                 <div class="center-page-content news-and-events container-boxes">
-
+                
+                
+                <a href="#" class="latest-news-event half-wrap  ">
+                        <div class="image-area">
+                            <img src="images/blog/transfermative-initiative.jpg" alt="Data Science">
+                        </div>
+                        <div class="news-event-detail">
+                            <div class="tag">NEWS</div>
+                            <div class="other-details">
+                                <div class="detail">Rector KSBL Mr. Mubashar Hameed conducts a townhall with employees about the latest transformation initiatives</div>
+                                <div class="date">Thursday, July 1st, 2021</div>
+                            </div>
+                        </div>
+                    </a>
 
                 <a href="#" class="latest-news-event half-wrap  ">
                         <div class="image-area">
