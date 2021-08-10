e0e7f49bf32aa9dc57dd5ea94cae85b14e4e8512
sajidkhattak1996 <Dev2.maven@gmail.com>
2021-08-10 16:32:19 +0500

    page create for new member added to faculty

---
+++ faculty-profile/Sheraz-Mustafa-Rajput.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$page_meta_key = 'faculty';
+include('../partials/head.php');
+
+?>
+
+<body class="mba aboutt bog main-faculty">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="faculty_grid">
+                <div class="faculty_grid_left dr-shoaib">
+                    <div class="faculty_grid_head">
+                        <h1>Sheraz Mustafa Rajput</h1>
+                        <h4>Associate Professor<br> Economics</h4>
+                        <a href="mail:to">Sheraz.mustafa@ksbl.edu.pk</a>
+                    </div>
+                </div>
+                <div class="faculty_grid_center">
+                    <ul class="nav nav-tabs border-0" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#vita">Profile Summary</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#credentials">Credentials</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#interests">Consulting & Teaching</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#publications">Publications & Research</a>
+                        </li>
+                    </ul>
+                    <!-- Tab panes -->
+                    <div class="tab-content mt-2">
+                        <div id="vita" class=" tab-pane active">
+                            <p><span style="color: #ff9f19;">Dr. Sheraz </span>
+                            is an Associate Professor of Economics at Karachi School of Business and Leadership (KSBL). He has over ten years of teaching and research experience. Before joining KSBL, he remained an integral part of faculty at Sukkur IBA University where he taught variety of Economics subjects. Apart from teaching, he has significantly contributed in administration as Economics Coordinator for Association to Advance Collegiate School of Business (AACSB) and National Business Education Accreditation Council (NBEAC). Further, taking the lead, he launched BS Economics Program at Sukkur IBA University which offers a wide range of specializations and equips students with prodigious academic and practical insights of local and global economies.
+                            </p>
+                            <p>Whilst pursuing his MS and PhD at the University of York, Dr. Sheraz developed skills as an empirical researcher and particularly as an applied econometrician. He has published a considerable volume of research at peer-reviewed journals. In particular, his research “Inequality and Composition of Taxes” published in International Tax and Public Finance is well recognized within the community of researchers. His research interests have broadened within the field of political economics particularly and conflict and environmental economics, finance and managerial practice generally.</p>
+                            <p>For shedding light on national and international socio-economic and political economic issues, Dr. Sheraz has conducted and moderated several seminars across the country. He believes in interacting with public and engaging in national and international debates.</p>
+
+                        </div>
+                        <div id="credentials" class=" tab-pane fade"><br>
+                            <!-- <h4 class="text-primary">Education</h4> -->
+                         
+                        </div>
+                        <div id="interests" class=" tab-pane fade"><br>
+                            
+                        </div>
+                        <div id="publications" class=" tab-pane fade"><br>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                    <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+                    
+                      <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq-e-Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br> Accounting & Finance<br>
+                                    Head of Department, Business Administration</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Abdul-Rahim-Nasir.php">Dr. Abdul Rahim Nasir
+                                <span>Professor<br>
+                                Business and Supply Chain Analytics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+                   
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waheed-Ali-Umrani.php">Dr. Waheed Ali Umrani
+                                <span>Associate Professor <br>HR & OB</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+                 
+                     
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Talha-Salam.php">Dr. Muhammad Talha Salam
+                                <span>Assistant Professor<br>Marketing & Analytics</span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Faryal-Razzaq.php">Dr. Faryal Razzaq
+                                <span>Assistant Professor<br>Management Sciences</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Asim-Khwaja.php">Dr. Asim Khwaja
+                                <span>Assistant Professor<br>Decision Sciences</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+</body>
+
+</html>
