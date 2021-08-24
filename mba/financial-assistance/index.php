4f79c3f705abab7dc980e99f5b0038a443dc8d22
ahtesham127 <ahtesham_hussain@outlook.com>
2021-08-24 22:04:03 +0500

    Updated financial page

--- mba/financial-assistance/index.php
+++ mba/financial-assistance/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <?php 
 $page_meta_key = 'MBA-Financial-Assistance';
@@ -70,51 +70,157 @@ include('../../partials/navbar.php');
     <!-----------------------------------------------------------------#
     #              Financial Assistance Start                          #
     #----------------------------------------------------------------->
-    <div class="mba-section financialassistance assistance">
+     <div class="content-section financialAssitTable">
         <div class="container">
-            <div class="left-side">
-                <img src="images/mba/financialassistance.jpg" alt="Financial Assistance" />
-            </div>
-            <div class="right-side">
-                <h2>Financial Assistance</h2>
-                <div class="detail">
-                    <p>KSBL endeavours to make business education accessible for everyone, irrespective of socio-economic standing, political views and ethnic background. Our needs blind admissions policy ensures that each application is assessed entirely on merit only. </p>
-                    <p>We make our programs accessible to deserving learners through our Scholarship and Financial Assistance program, which includes the following:
-                        <br>1. Merit Scholarships
-                        <br>2. KSBL Grants
-                        <br>3. Third Party Loans (Qarz-e-Hasana)
-                        <br>4. Income Share Agreements
-                        <br>5. Research Assistantships and Campus Jobs
-                    </p>
+            <div class="content-area">
+                <div class="admission-process">
+                    <div class="tabs">
+                        <div class="tab active" data-tab="Types_Financial_Assistance">Types of Financial Assistance</div>
+                        <div class="tab" data-tab="Assessment_Process">Need Assessment Process</div>
+                        <div class="tab" data-tab="Terms_and_Conditions">Terms and Conditions</div>
+                        <div class="tab" data-tab="Document_Checklist">Need Assessment Document Checklist</div>
+                    </div>
+                    <div class="tabs-content">
+                        <!-- ==== eligibility === -->
+                        <div class="tab-content active" data-tab="Types_Financial_Assistance">
+                            <div class="financialTable_content">
+                                <h4 class="heading">KSBL aspire to attract the top talent and believes that financial impediments should not be a barrier to be part of the premier educational programs offered at KSBL. We offer Financial Assistance in the form of</h4>
+                                <ul class="list-unstyled">
+    <li>i.  Merit Scholarships</li>
+    <li>ii. KSBL Need Based Grants</li>
+    <li>iii.    Third Party Loans</li>
+    <li>iv. Paid Teaching/Research Assistance-ship &amp; Student Jobs  </li>
+    <li>v.  Alumni Discounts</li>
+    <li>vi. Institutional Discounts</li>
+    <li>vii.    Special Quota for Females in Undergrad Scholarships, Grants and Student Loans.</li>
+</ul>
+                              
+                            </div>
+                           
+                            
+                        </div>
+                        <!-- ==== deadlines === -->
+                        <div class="tab-content" data-tab="Assessment_Process">
+                            <div class="sec-row">
+                                <strong>Admissions Open MEBA, IT&S, MBA, Executive MBA, MSBA </strong>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>
+                                      Admissions Early Bird Deadline
+                                    </strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <strong>Sep 10, 2021</strong>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Admissions Final Deadline</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <strong>Sep 20, 2021</strong>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>KMAT, KUAT / SAT based Admission Test</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <strong>Rolling Basis*</strong>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Fall Semester Start Date</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <strong>Oct 04, 2021</strong>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="sec-row">
+                            *Please email KSBL Admissions for KUAT and KMAT Dates: admissions@ksbl.edu.pk
+                            </div>
+
+                        </div>
+                        <!-- ==== checklist === -->
+                        <div class="tab-content" data-tab="Terms_and_Conditions">
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Checklist Criteria</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <li>Online Admission Application Form</li>
+                                        <li>Copy of Official Transcripts/Mark Sheets - Degree/Certificates (Last three qualifications, Latest and two previous)</li>
+                                        <li>Copy of Test Score - KMAT (KSBL Management Admissions Test), GMAT/GRE. In case of yet to appear for a test, scheduled test dates for any of the above needs to be provided</li>
+                                        <li>Resume/CV</li>
+                                        <li>Copy of previous or current employment/Internship certificates</li>
+                                        <li>Copy of Computerized National Identity Card (CNIC) or copy of valid Passport</li>
+                                        <li>Two Personal Statement Essays</li>
+                                        <li>Two Letters of Recommendations</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Special Announcement</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        <li>The admissions application is now online and all documents may be uploaded online for further assessment</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                            </div>
+                        </div>
+                        <!-- ==== Process === -->
+                        <div class="tab-content" data-tab="Document_Checklist">
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Online Application</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        Online Application - <a href="http://admissions.colaraz.com/signup?institute=ksbledu" target="_blank">Click here</a> to proceed <br>For the online application process, you are expected to submit all the required documents</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Processing Fee</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        A processing fee of PKR 5000 will be charged, which will also include the fee for KMAT
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Online Fee Submission</strong></div>
+                                <div class="sec-col">
+                                    The applicants can submit the application processing fee online at the following KSBL account:<br>
+                                    <br><strong>Account Title:</strong> Karachi School of Business and Leadership
+                                    <br><strong>Account number:</strong> 0010030887260010
+                                    <br><strong>IBAN:</strong> PK81ABPA0010030887260010
+                                    <br><strong>SWIFT:</strong> ABPAP
+                                    <br><strong>BIC:</strong> N/A
+                                    <br><strong>Sort Code:</strong> N/A
+                                    <br><strong>Bank Address:</strong> Allied Bank Limited Stadium Road Branch (0971) Karachi
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                                <div class="sec-col heading"><strong>Essentials</strong></div>
+                                <div class="sec-col">
+                                    <ul>
+                                        Applicants are required to submit proof of payment of processing fee on the online admissions portal for their application to be processed
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="sec-row">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+          
         </div>
     </div>
-    <!-----------------------------------------------------------------#
-    #                Financial Assistance END                         #
-    #----------------------------------------------------------------->
-    <!-----------------------------------------------------------------#
-    #              Application Assessment Process Start                #
-    #----------------------------------------------------------------->
-    <div class="mba-section applicationassistance assistance">
-        <div class="container">
-            <div class="left-side">
-                <img src="images/mba/application-assistance-process.jpg" alt="Application Assessment Process" />
-            </div>
-            <div class="right-side">
-                <h2>Application Assessment Process</h2>
-                <div class="detail">
-                    <p>We have a committee consisting of both management and faculty, which analyzes each case in detail. We determine if the candidate's family has adequate resources to support the program. We base our decision on the financial credentials and other family particulars that the applicants submit. </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-----------------------------------------------------------------#
-    #                      Faculty MBA END                             #
-    #----------------------------------------------------------------->
-    <!-----------------------------------------------------------------#
-    #                      Open House Start                            #
-    #----------------------------------------------------------------->
     <div class="open-house-section  new-open-houese">
         <div class="container">
             <div class="event-program">
