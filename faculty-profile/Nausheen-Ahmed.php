a9e66e0c6f653be4f18a3b8dbb6dffd53794fa9b
sajidkhattak1996 <sajidktkkpk@gmail.com>
2021-08-24 12:06:20 +0500

    new page created for new member

---
+++ faculty-profile/Nausheen-Ahmed.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+include('../partials/head.php');
+
+?>
+<style type="text/css">
+body,
+html {
+
+    overflow-x: hidden;
+
+}
+</style>
+
+<body class="mba about bog">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.html" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.html" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../faculty.html" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.html" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+
+    #                   MBA Department END                             #
+
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+
+    #                     Faculty Group Start                          #
+
+    #----------------------------------------------------------------->
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="non_faculty_grid">
+                <div class="non_faculty_grid_left">
+                    <div class=" non_faculty_grid_img  non_faculty_grid_img_Nausheen_Ahmed">
+                        <div class="non_faculty_grid_head">
+                            <h1>Nausheen Ahmed</h1>
+                            <h4>Business Law</h4>
+                        </div>
+                    </div>
+                    <div class="non_faculty_grid_two">
+                        <div class="non_faculty_grid_two_left">
+                            <h4>Recent Activity</h4>
+                            <!-- <div class="recent_activity">
+                                <a href="">Mubashar Speaks on the orientation for the Inaugral batch of KSBL MS Business Analytics. </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href=""><b>Mubashar welcomes Mr. Shoaib Khalil,</b> Head of Channel & Business Development, Microsoft Pakistan, for a session at KSBL on Future Job Trends... </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href="">The board of KSBL has appointed Mr. Mubashar Hameed as the Rector.
+                                    In addition to his role as Rector KSBL, Mr. Hameed will also serve as CEO of Karachi Education Initiative (KEI). </a>
+                                <span>Tuesday, May 12th, 2020</span>
+                            </div> -->
+                        </div>
+                        <div class="non_faculty_grid_two_right">
+                            <div class="non_faculty_grid_two_right_content">
+                                <p><b>Nausheen Ahmad</b> serves as Independent Director of Meezan Bank Ltd, Jubilee General Insurance Co Ltd, International Steel Ltd and is a member of the Pay and Pension Commission (Government of Pakistan). She recently completed a tenure as independent director of the Pakistan Stock Exchange (having been appointed against a casual vacancy). </p>
+                                <p>Having spent over three decades in the corporate sector, Nausheen brings with her a tremendous amount of experience and expertise in the legal and regulatory area. Nausheen began her legal practice with the corporate law firm Surridge and Beecheno. Later, she went on to hold the position of legal counsel at Pakistan Petroleum Ltd and Unilever Pakistan Ltd. Later with ICI Pakistan Ltd and HBL she was the Company Secretary and General Counsel.</p>
+                                <p>Nausheen has an LLM degree from the University of London, an LLB from Kings College London, and a degree in the Philosophy of Religion from Kings College London. She was called to the Bar from the Honourable Society of Grays Inn London and is registered as an Advocate of the Sindh High Court. Nausheen was also accredited as a mediator and master trainer by the Centre for Effective Dispute Resolution, UK.</p>
+                                <p>With her strong interest in corporate governance, Nausheen delivers executive training with the Pakistan Mediators Association, the Institute of Business Administration (CEE), and Pakistan Institute of Corporate Governance (PICG). Recently, she joined the Institute of Business Management (IOBM) as visiting faculty teaching business and company law. </p>
+                                <p>She is a founder of the Dispute Resolution Forum at IBA, Karachi which promotes civil and commercial mediation. This has become an important area of practice as the law has been amended in Sindh province to enable referral and recognition of mediation.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                    <h4>Founders & Trustees</h4>
+                    <div class="non_faculty_grid_right_scroll">
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Hussain Dawood
+                                <span>Founding Chairman, Board of Governors,<br>
+                                    KSBL</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Munawar Alam Siddiqui
+                                <span>Director & Chair<br>
+                                    Infrastructure Committee Chairman<br>
+                                    J.S Investments Limited
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Muhammad Ali Tabba
+                                <span>Director, KEI<br>
+                                    CEO, Lucky Cement Limited
+                                    KSBL</span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Nauman Ansari
+                                <span>Director, KEI<br>
+                                    Director & CEO, Bank Alfalah
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Sabrina Dawood
+                                <span>Director, KEI<br>
+                                    Director, Dawood Hercules Corporation Ltd
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Arif Habib
+                                <span>Director, KEI<br>
+                                    Director & Chair<br>
+                                    Finance & Fundraising Committee
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Henna Inam
+                                <span>Director, KEI<br>
+                                    Director, Engro Corporation<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="">Hasan Irfan Khan
+                                <span>Director, KEI<br>
+                                    Director, Engro Corporation<br>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
