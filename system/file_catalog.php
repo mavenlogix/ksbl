428e3dfad11b3e15d5a48df75386a662a20a6e16
ahtesham127 <ahtesham_hussain@outlook.com>
2021-11-21 19:07:55 +0500

    Update file_catalog.php

--- system/file_catalog.php
+++ system/file_catalog.php
@@ -84,7 +84,12 @@ class File_Catalog
     public function setcookie()
     { 
 
-        echo "<script>document.cookie = ' ". $this->cookie_name."=" . base64_encode(array_key_last($this->json)."; expires=Sat, 18 Dec 2021 12:00:00 UTC; path=/';</script>"
+        echo "<script>document.cookie = '  
+
+        ". $this->cookie_name."=" . base64_encode(array_key_last($this->json)."; expires=Sat, 18 Dec 2021 12:00:00 UTC;   
+
+
+            ';</script>"
         
 
     }
