c0d71e62356790e3cf518dc4b066c27313c53317
ahtesham127 <ahtesham_hussain@outlook.com>
2021-11-21 19:29:27 +0500

    Update file_catalog.php

--- system/file_catalog.php
+++ system/file_catalog.php
@@ -83,7 +83,16 @@ class File_Catalog
     }
     public function setcookie()
     { 
-        setcookie($this->cookie_name, base64_encode(array_key_last($this->json)), strtotime('+6 months'), '', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
+
+         $cookieName = $this->cookie_name;
+         $cookieValue = base64_encode(array_key_last($this->json));
+         $path = stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/');
+
+        // setcookie($cookieName, $cookieValue, strtotime('+6 months'), '', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
+     
+         
+ echo "<script>document.cookie = '".$cookieName.'='.$cookieValue.'; expires=Sat, 18 Dec 2021 12:00:00 UTC; path='.$path."'</script>";
+
 
     }
     public function reset()
