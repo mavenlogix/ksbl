fd1a5fe881a8349df312d3755ec02e51b5e17a9b
ahtesham127 <ahtesham_hussain@outlook.com>
2021-11-22 21:38:24 +0500

    y

--- partials/footer-scripts.php
+++ partials/footer-scripts.php
@@ -2,10 +2,12 @@
   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="<?= $url ?>js/jquery-ui/jquery-ui.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="js/owl.carousel.js"></script>
     <script src="js/script.js"></script>
+    
 
    <!--  <script src="js/script-faculty.js"></script> -->
    <script>
