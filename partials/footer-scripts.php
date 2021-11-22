1898f7983f589211e125d47f967c7142623b3f56
ahtesham127 <ahtesham_hussain@outlook.com>
2021-11-22 21:50:07 +0500

    Update footer-scripts.php

--- partials/footer-scripts.php
+++ partials/footer-scripts.php
@@ -2,7 +2,7 @@
   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="<?= $url ?>js/jquery-ui/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.js"></script>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="js/owl.carousel.js"></script>
