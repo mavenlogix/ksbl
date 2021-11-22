308a036adbc108df00e258ba56697557591bf246
ahtesham127 <ahtesham_hussain@outlook.com>
2021-11-22 21:11:18 +0500

    Update file_catalog.php

--- system/file_catalog.php
+++ system/file_catalog.php
@@ -83,13 +83,13 @@ class File_Catalog
     }
     public function setcookie()
     {
-         $cookieName = $this->cookie_name;
-          $cookieValue = base64_encode(array_key_last($this->json));
-         $path = stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/');
-         $path = "/";
- echo "<script>window.onload = function(e){  document.cookie = '".$cookieName.'='.$cookieValue.'; expires=Sat, 18 Dec 2021 12:00:00 UTC; path='.$path."' }</script>";
+ //         $cookieName = $this->cookie_name;
+ //          $cookieValue = base64_encode(array_key_last($this->json));
+ //         $path = stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/');
+ //         $path = "/";
+ // echo "<script>window.onload = function(e){  document.cookie = '".$cookieName.'='.$cookieValue.'; expires=Sat, 18 Dec 2021 12:00:00 UTC; path='.$path."' }</script>";
 
-        // setcookie($this->cookie_name, rtrim(base64_encode(array_key_last($this->json)), '='), strtotime('+6 months'), '/', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
+         Cookie.set($this->cookie_name, rtrim(base64_encode(array_key_last($this->json)), '='), strtotime('+6 months'), '/', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
         ob_end_flush();
     }
     public function reset()
