efe24d5c5d21408fc9fe1a4037635aba999d3815
Raza <ahmed.r.qadri@gmail.com>
2021-11-22 08:50:46 +0500

    buffer output until cookie is set to prevent header sent error

--- system/file_catalog.php
+++ system/file_catalog.php
@@ -83,16 +83,7 @@ class File_Catalog
     }
     public function setcookie()
     {
-        setcookie($this->cookie_name, base64_encode(array_key_last($this->json)), strtotime('+6 months'), '', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
-
-             //$cookieName = $this->cookie_name;
-          //$cookieValue = base64_encode(array_key_last($this->json));
-         // $path = stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/');
-         // $path = "/";
-
-         
- // echo "<script>document.cookie = '".$cookieName.'='.$cookieValue.'; expires=Sat, 18 Dec 2021 12:00:00 UTC; path='.$path."'</script>";
-
+        setcookie($this->cookie_name, rtrim(base64_encode(array_key_last($this->json)), '='), strtotime('+6 months'), '/', stripos(SCRIPT_BASE, 'localhost') !== false ? 'localhost' : trim('.' . str_ireplace('www.', '', SCRIPT_BASE), '\\\/'));
     }
     public function reset()
     {
