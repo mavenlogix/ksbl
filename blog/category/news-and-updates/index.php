ddd25031761912a78cce770092b7fe38d496b3b4
sajidkhattak1996 <dev2.maven@gmail.com>
2021-11-30 17:27:35 +0500

    temporally try to show badge for client

--- blog/category/news-and-updates/index.php
+++ blog/category/news-and-updates/index.php
@@ -50,6 +50,7 @@ include('../../../partials/navbar.php');
                         </div>
                         <div class="news-event-detail">
                             <div class="tag">NEWS</div>
+                            <span class="badge">New</span>
                             <div class="other-details">
                                 <div class="detail" >KSBL and iQualify UK signed a Memorandum of Understanding (MOU)</div>
                                 <div class="date">Wednesday, November , 17th, 2021</div>
