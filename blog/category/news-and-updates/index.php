e2a70751571d96a18757f0d01cad6f8cfc79d5dd
sajidkhattak1996 <dev2.maven@gmail.com>
2021-12-10 12:49:54 +0500

    new event added

--- blog/category/news-and-updates/index.php
+++ blog/category/news-and-updates/index.php
@@ -42,7 +42,21 @@ include('../../../partials/navbar.php');
 
                 <div class="center-page-content news-and-events container-boxes" >
 
-               
+                
+
+                <a href="blog/Nai_Subah.php" class="latest-news-event half-wrap  " >
+                        <div class="image-area">
+                            <img src="images/blog/Nai_Subah_thumbnail.jpg" alt="Data Science">
+                        </div>
+                        <div class="news-event-detail">
+                            <div class="tag">Events <?=  $catalog->check() ? '<span class="badge">New</span>' : null ?></div>
+                          
+                            <div class="other-details">
+                                <div class="detail" >KSBL Hosted an Event Organized by "Nai Subah"</div>
+                                <div class="date">Saturday, December, 4th, 2021</div>
+                            </div>
+                        </div>
+                    </a>
 
                     <a href="blog/Karim_Jivani_Data_Science_and_Analytics.php" class="latest-news-event half-wrap  " >
                         <div class="image-area">
