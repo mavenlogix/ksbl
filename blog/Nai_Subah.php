2d18a43000e2b2b2ac3fc7e84eba78a447672c36
sajidkhattak1996 <dev2.maven@gmail.com>
2021-12-21 14:23:09 +0500

    file added

---
+++ blog/Nai_Subah.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$page_meta_key = 'News-and-Events';
+include('../partials/head.php');
+
+?>
+
+<body class="mba open-house center-page category-page">
+    <!-----------------------------------------------------------------#
+	#                       Header START                               #
+	#----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+           <?php
+include('../partials/navbar.php'); 
+?>
+            <div class="header-content">
+                <a href="index.php" title="KSBL" class="logo">
+                    <img src="images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+            </div>
+            <div class="open-house-content single-blog">
+                <h1>News & EVENTS . 2021</h1>
+                <div class="page-detail">Stay up to date with the latest happenings at KSBL</div>
+
+                <a href="blog/category/news-and-updates/" class="back-link" title="All News">All News & Events</a>
+                <div class="center-page-content news-and-events container-boxes">
+                    <div class="blog-header">
+                        <img src="images/blog/Nai_Subah_thumbnail.jpg" alt="Blog other">
+                        <div class="blog-name">
+                            <div class="tag">Events</div>
+                            <h2>KSBL Hosted an Event Organized by "Nai Subah"</h2>
+                            <div class="post-date">Saturday, December, 4th, 2021</div>
+                        </div>
+                    </div>
+                    <div class="blog-detail">
+                        <p>
+                        KSBL under its CSR wing hosted an event organized by "Nai Subah" for the empowerment of visually impaired youth at KSBL campus on December 4th, 2021, in honour of International Disability Day. People from all walks of life attended the event.
+                        </p>
+                   
+
+                    </div>
+                    <!-----------------------------------------------------------------#
+							#                       Other Pictures                             #
+							#----------------------------------------------------------------->
+                    <div class="other-pictures">
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img1.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img2.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img3.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img4.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img5.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img6.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img7.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                        <div class="blog-image-section image">
+                            <div class="campus-image">
+                                <img src="images/blog/Nai_Subah_img8.jpg" alt="Data Science">
+                            </div>
+                        </div>
+                     
+                    </div>
+                    <!-----------------------------------------------------------------#
+							#                      END  Other Pictures                         #
+							#----------------------------------------------------------------->
+                </div>
+                <a href="blog/category/news-and-updates/" class="back-link" title="Back to News">Back to News & Events</a>
+            </div>
+        </div><!-- container -->
+    </header>
+    <!-----------------------------------------------------------------#
+	#                       Header END                                 #
+	#----------------------------------------------------------------->
+    
+<?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+    <script async defer src="https://cse.google.com/cse.js?cx=5a67611eb2aef8e92"></script>
+</body>
+
+</html>
