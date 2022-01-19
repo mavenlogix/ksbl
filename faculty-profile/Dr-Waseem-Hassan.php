7c63a5c6c04a2acba543a1c4fae3f5ca7f182c1e
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 12:23:18 +0500

    Dr waseem profile content updated.

--- faculty-profile/Dr-Waseem-Hassan.php
+++ faculty-profile/Dr-Waseem-Hassan.php
@@ -79,114 +79,115 @@ include('../partials/navbar.php');
                     <div class="tab-content mt-2">
                         <div id="vita" class=" tab-pane active">
                             <p><span style="color: #ff9f19;">Dr. Waseem Hassan </span>
-                            has a distinguished career as a teacher, researcher, mentor and consultant. He has done his PhD from China University of Geosciences, Wuhan, P.R. China and has been actively involved in research and exchange activities with different Chinese universities and organizations.
+                            is an Associate Professor of Marketing Strategy at KSBL. He has several years of teaching experience at National University of Science & Technology (NUST), Business School, Islamabad and other reputed institutes in Pakistan and China.
                             </p>
                             <p>
-                            Before joining KSBL, Dr. Hassan was associated with NUST Business School, NUST, Islamabad. He performed the duties of Assistant Professor, Program Head MBA, Head of Department of International Business & Marketing, Head Alumini Linkages, and Head Industrial Linkages. He has also arranged visits of Chinese delegations to NUST and other institutes. He has played an important role in bringing NUST Business School among top 300 Business Schools in QS World Ranking, securing five years NBEAC accreditation and securing AACSB membership. 
-                            </p>
-
-                            <p>
-                            Dr. Hassan is also associated with HEC as member HEC Accreditation Committee and have visited several institutes across the country for accreditation. He has profound knowledge about HEC’s rules, regulations and quality standards and always emphasize on quality education. 
+                            At NUST he served as the Assistant Professor, Program Head MBA, Head of Department of International Business & Marketing, Head Alumni Linkages, and Head Industrial Linkages.  
                             </p>
 
                             <p>
-                            Dr. Hassan has published several articles in well reputed journals and conferences. His area of interest is Marketing, Brand Management, Digital Marketing Platforms and Consumer Behavior. He has a good understanding and experience of teaching with different pedagogies. He has experience of teaching in different institutes in Pakistan and China.  
+                            He is also associated with Higher Education Commission (HEC) as member HEC Accreditation Committee and has visited several institutes across the country for accreditation. 
+                            </p>
+
+                            <p>
+                            Dr. Hassan has done his Ph.D. from China University of Geosciences, Wuhan, China and has been actively involved in research and exchange activities with different Chinese universities and organizations. 
                             </p>
                             
                         </div>
                         <div id="credentials" class=" tab-pane fade"><br>
                           <p>
-                              <b>
-                                PhD, China University of Geosciences, Wuhan, China<br>
-                                Marketing<br>
+                             <ul>
+                             <li>
+                                 Ph.D.  China University of Geosciences, Wuhan, China <br>
+                                Marketing <br>
                                 2015
-                              </b>
-                          </p>
-
-                          <p>
-                              <b>
-                                MBA, Islamia University, Bahawalpur, Pakistan<br>
-                                Marketing<br>
-                                2007 
-                              </b>
+                            </li>
+                            <li>
+                                MBA, Islamia University, Bahawalpur, Pakistan <br>
+                                Marketing <br>
+                                2007
+                            </li>
+                             </ul>
                           </p>
                           
                         </div>
                         <div id="interests" class=" tab-pane fade"><br>
                             <p>
                                 <ul>
-                                   <li>Head Of Department International Business & Marketing – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Feb 2019 till Sep 2021)</li>
-                                   <li>Program Head MBA – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Oct 2017- Jan 2019 )</li>
-                                   <li>Assistant Professor – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Sep 2016 till date)</li>
-                                   <li>Assistant Professor – Institute of Management Sciences, Bahauddin Zakariya University, Multan – Pakistan (Sep 2015 till Sep 2016)</li>
-                                   <li>Visiting Faculty – School of Management Studies at Wuhan University, PR. China</li>
-                                   <li>Research Assistant – School of Economics and Management at China University of Management Sciences (Wuhan), PR. China.</li>
+                                    <li>
+                                        Head Of Department International Business & Marketing – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Feb 2019 till Sep 2021)
+                                    </li>
+                                    <li>
+                                        Program Head MBA – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Oct 2017- Jan 2019 )
+                                    </li>
+                                    <li>
+                                        Assistant Professor – NUST Business School (NBS), National University of Sciences and Technology (NUST), Islamabad – Pakistan (Sep 2016 till date)
+                                    </li>
+                                    <li>
+                                        Assistant Professor – Institute of Management Sciences, Bahauddin Zakariya University, Multan – Pakistan (Sep 2015 till Sep 2016)
+                                    </li>
+                                    <li>
+                                        Visiting Faculty – School of Management Studies at Wuhan University, PR. China
+                                    </li>
+                                    <li>
+                                        Research Assistant – School of Economics and Management at China University of Management Sciences (Wuhan), PR. China.
+                                    </li>
                                 </ul>
                             </p>
                         </div>
                         <div id="publications" class=" tab-pane fade"><br>
                            <p>
-                            Wenyan Pan, Muhammad Awais Gulzar, <b>Waseem Hassan</b>, Synthetic Evaluation of China’s Regional Low-Carbon Economy Challenges by Driver-Pressure-State-Impact-Response Model, International Journal of Environmental Research and Public Health, July 2020. <b>Impact Factor: 2.85.</b>
+                               <ul>
+                                    <li>
+                                        Wenyan Pan, Muhammad Awais Gulzar, Waseem Hassan, Synthetic Evaluation of China’s Regional Low-Carbon Economy Challenges by Driver-Pressure-State-Impact-Response Model, International Journal of Environmental Research and Public Health, July 2020. Impact Factor: 2.85.
+                                        </li>
+                                    <li>
+                                        Muhammad Awais Gulzar, Haroon Asghar, Jinsoo Hwang, Waseem Hassan, China’s Pathway towards Solar Energy Utilization: Transition to a Low-Carbon Economy, International Journal of Environmental Research and Public Health, June 2020. Impact Factor: 2.85.
+                                        </li>
+                                    <li>
+                                        Hussain Tariq, Qinxiong Weng, Thomas Noel Garavan, Asfia Obaid, Waseem Hassan, Another Sleepless Night: Does a Leader’s Poor Sleep Leads to Subordinate’s Poor Sleep? A Spillover/Crossover Perspective, Journal of Sleep Research, August 2019. Impact Factor: 3.45.
+                                        </li>
+                                    <li>
+                                        Waseem Hassan, Rizwan Shabbir, Mohsin Bashir, Javed Akram, Electronic Surveillance Consequences, The Dark Side of Technology at Workplace, Pacific Business Review International, March 2019; 11(9) 157-170.
+                                        </li>
+                                    <li>
+                                        Waseem Hassan, Rizwan Shabbir, Sarah Imran, Effect of Word of Mouth on Customer Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Household Appliances Industry in China; Science International (Lahore), 2016; 28(2)1603-1610.
+                                        </li>
+                                    <li>
+                                        Shabbir, R., Zhang, J., Pitsaphol, C., & Hassan, W. (2015). The Dynamics of Value Creation in Online Brand Communities: Strategy Map Approach. Information Management and Business Review, 7 (1), 48-58.
+                                        </li>
+                                    <li>
+                                        J. Zhang, R. Shabbir1, C. Pitsaphol, W. Hassan; Managing Consumer Commitment Through Online Brand Communities: Empirical Evidence from China; European Scientific Journal, 2015;11 (1)138-154.
+                                        </li>
+                                    <li>
+                                        J. Zhang, R. Shabbir1, C. Pitsaphol, W. Hassan, Creating Brand Equity by Leveraging Value Creation and Consumer Commitment in Online Brand Communities: A Conceptual Framework; International Journal of Business and Management;2015; 10(1)80-91.
+                                        </li>
+                                    <li>
+                                        W. Hassan, Sh. Chuanmin, F. Ghazanfar, Muhammad A. Gulzar, R. Shabbir. Customer cognitive factors on brand trust: a study of Pakistan's household appliances industry; International Journal of Academic Research Part B; 2014; 6(6), 309-315.
+                                        </li>
+                                    <li>
+                                        W. Hassan, Sh. Chuanmin, R. Shabbir, Muhammad A. Gulzar. Customer cognitive factors on brand trust: a study of China's household appliances industry; International Journal of Academic Research Part B; 2014; 6(6), 321-328.
+                                        </li>
+                                    <li>
+                                        W. Hassan, Sh. Chuanmin Muhammad A. Gulzar, F. Ghazanfar, R. Shabbir, Cognitive Dissonance Creation by Cultural and Psychological Factors and Its Effects on Consumer Decision Making Process; Journal of Research on Humanities and Social Sciences, 2014; 4(8)37-41.
+                                        </li>
+                                    <li>
+                                        Maria Shahid, Seong Jae Ming, W. Hassan, Impact of Online Cross Cutting Exposure on Consumer Political Participation and Social Anxiety. <br>
+                                    Academy of Marketing Conference 2019. <br>
+                                    Organized at Regents University, London
+                                    </li>
+                                    <li>
+                                        W. Hassan, R. Shabbir, S. Imran, Impact of After Sale Services on Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Home Appliances Industry in Pakistan; <br>
+                                    4th International Conference on Business, Marketing and Management <br>
+                                    Organized at Harvard University, Boston, United States of America
+                                    </li>
+                                    <li>
+                                        W. Hassan, R. Shabbir, S. Imran, Effect of Brand Popularity on Customer Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Household Appliances Industry in Pakistan; <br>
+                                        3rd International Conference on Marketing <br>
+                                        Organized by University of Malaya, Kuala Lumpur – Malaysia/IBA. Karachi – Pakistan
+                                    </li>
+                               </ul>
                            </p>
-
-                           <p>
-                           Muhammad Awais Gulzar, Haroon Asghar, Jinsoo Hwang, <b>Waseem Hassan</b>, China’s Pathway towards Solar Energy Utilization: Transition to a Low-Carbon Economy, International Journal of Environmental Research and Public Health, June 2020. <b>Impact Factor: 2.85.</b>
-                           </p>
-
-                           <p>
-                           Hussain Tariq, Qinxiong Weng, Thomas Noel Garavan, Asfia Obaid, <b>Waseem Hassan</b>, Another Sleepless Night: Does a Leader’s Poor Sleep Leads to Subordinate’s Poor Sleep? A Spillover/Crossover Perspective, Journal of Sleep Research, August 2019. <b>Impact Factor: 3.45.</b>
-                           </p>
-
-                           <p>
-                           <b>Waseem Hassan</b>, Rizwan Shabbir, Mohsin Bashir, Javed Akram, Electronic Surveillance Consequences, The Dark Side of Technology at Workplace, Pacific Business Review International, March 2019; 11(9) 157-170.
-                           </p>
-
-                           <p>
-                           <b>Waseem Hassan</b>, Rizwan Shabbir, Sarah Imran, Effect of Word of Mouth on Customer Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Household Appliances Industry in China; Science International (Lahore), 2016; 28(2)1603-1610.
-                           </p>
-
-                           <p>
-                           Shabbir, R., Zhang, J., Pitsaphol, C., & <b>Hassan, W</b>. (2015). The Dynamics of Value Creation in Online Brand Communities: Strategy Map Approach. Information Management and Business Review, 7 (1), 48-58.
-                           </p>
-
-                           <p>
-                           J. Zhang, R. Shabbir1, C. Pitsaphol, <b>W. Hassan</b>; Managing Consumer Commitment Through Online Brand Communities: Empirical Evidence from China; European Scientific Journal, 2015;11 (1)138-154.
-                           </p>
-
-                           <p>
-                           J. Zhang, R. Shabbir1, C. Pitsaphol, <b>W. Hassan,</b> Creating Brand Equity by Leveraging Value Creation and Consumer Commitment in Online Brand Communities: A Conceptual Framework; International Journal of Business and Management;2015; 10(1)80-91.
-                           </p>
-
-                           <p>
-                           <b>W. Hassan,</b> Sh. Chuanmin, F. Ghazanfar, Muhammad A. Gulzar, R. Shabbir. Customer cognitive factors on brand trust: a study of Pakistan's household appliances industry; International Journal of Academic Research Part B; 2014; 6(6), 309-315.
-                           </p>
-
-                           <p>
-                           <b>W. Hassan</b>, Sh. Chuanmin, R. Shabbir, Muhammad A. Gulzar. Customer cognitive factors on brand trust: a study of China's household appliances industry; International Journal of Academic Research Part B; 2014; 6(6), 321-328.
-                           </p>
-
-                           <p>
-                           <b>W. Hassan</b>, Sh. Chuanmin Muhammad A. Gulzar, F. Ghazanfar, R. Shabbir, Cognitive Dissonance Creation by Cultural and Psychological Factors and Its Effects on Consumer Decision Making Process; Journal of Research on Humanities and Social Sciences, 2014; 4(8)37-41.
-                           </p>
-
-                           <p>
-                            Maria Shahid, Seong Jae Ming, <b>W. Hassan</b>, Impact of Online Cross Cutting Exposure on Consumer Political Participation and Social Anxiety.
-                            <br><b>Academy of Marketing Conference 2019.</b><br>
-                            <i>Organized at Regents University, London</i>
-                           </p>
-
-                           <p>
-                            <b>W. Hassan</b>, R. Shabbir, S. Imran, Impact of After Sale Services on Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Home Appliances Industry in Pakistan;
-                            <br><b>4<sup>th</sup> International Conference on Business, Marketing and Management</b><br>
-                            <i>Organized at Harvard University, Boston, United States of America</i>
-                           </p>
-
-                           <p>
-                           <b>W. Hassan</b>, R. Shabbir, S. Imran, Effect of Brand Popularity on Customer Pre-Purchase Dissonance, Customer Satisfaction and Brand Trust: A Study of Household Appliances Industry in Pakistan; 
-                            <br><b>3<sup>rd</sup> International Conference on Marketing</b><br>
-                            <i>Organized by University of Malaya, Kuala Lumpur – Malaysia/IBA. Karachi – Pakistan</i>
-                           </p>
-
 
                         </div>
                     </div>
