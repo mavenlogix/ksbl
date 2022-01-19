9e5164cf34d46f9b90d9c3e5d7edb5f4ee9660d3
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 17:10:24 +0500

    Danish haroon profile created

---
+++ faculty-profile/Mr_Danish_Haroon.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+include('../partials/head.php');
+
+?>
+<style type="text/css">
+body,
+html {
+
+    overflow-x: hidden;
+
+}
+</style>
+
+<body class="mba about bog">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.php" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+
+    #                   MBA Department END                             #
+
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+
+    #                     Faculty Group Start                          #
+
+    #----------------------------------------------------------------->
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="non_faculty_grid">
+                <div class="non_faculty_grid_left">
+                    <div class=" non_faculty_grid_img  non_faculty_grid_img_mr_shahrukh_khan">
+                        <div class="non_faculty_grid_head">
+                            <h1>Mr. Danish Haroon</h1>
+                            <h4>Data Mining and Big Data</h4>
+                        </div>
+                    </div>
+                    <div class="non_faculty_grid_two">
+                        <div class="non_faculty_grid_two_left">
+                            <h4>Recent Activity</h4>
+                            <!-- <div class="recent_activity">
+                                <a href="">Mubashar Speaks on the orientation for the Inaugral batch of KSBL MS Business Analytics. </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href=""><b>Mubashar welcomes Mr. Shoaib Khalil,</b> Head of Channel & Business Development, Microsoft Pakistan, for a session at KSBL on Future Job Trends... </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href="">The board of KSBL has appointed Mr. Mubashar Hameed as the Rector.
+                                    In addition to his role as Rector KSBL, Mr. Hameed will also serve as CEO of Karachi Education Initiative (KEI). </a>
+                                <span>Tuesday, May 12th, 2020</span>
+                            </div> -->
+                        </div>
+                        <div class="non_faculty_grid_two_right">
+                            <div class="non_faculty_grid_two_right_content">
+                                <p><b>Danish Haroon</b> is a visiting faculty of Data Mining and Big Data at KSBL. He has over 6 years of experience in the data science space, spanning along the verticals of retail, banking, and utility.
+                                </p>
+                                <p>
+                                He has worked in consulting roles for multiple offshore clients, and delivered data driven products within the local market as well. During his experience at K Electric, he has built a team from scratch, providing value to business units by building end-to-end machine learning pipelines.
+                                </p>
+                                <p>
+                                He is an MBA from KSBL and did his MS Data Science from FAST NUCES. He is associated with a research group working on tackling with the issues in long tailed tabular data, and aspires to pursue his Ph.D. along the same lines.
+                                </p>
+
+                              
+
+                                <p><h4 class="text-primary">Credentials</h4></p>
+                                <p>
+                                    <b>Education</b>
+                                    <br>
+                                    <ul>
+                                        <li>
+                                            MS in Data Science, FAST NUCES, Pakistan
+                                            Thesis Title “Learning a better representation while retaining classification performance on imbalanced data” 2021
+                                        </li>
+                                        <li>
+                                            MBA, KSBL
+                                            Research Title "Feasibility study for launching a water-displacing spray in the local market"
+                                            2015
+                                        </li>
+                                        <li>
+                                            BS Telecommunication Engineering
+                                            2012
+                                        </li>
+                                    </ul>
+                                </p>
+                            
+
+                                <h4 class="text-primary">Consulting and Teaching</h4>
+                                <p>
+                                    2019 – Present 	 <br>
+                                    Manager - Analytics, <br>
+                                    K Electric
+                                </p>
+                                <p>
+                                    2018 - 2019	 <br>	
+                                    Data Scientist Consultant <br>
+                                    qordata
+                                </p>
+                                <p>
+                                    2016- 2018 <br>	
+                                    Data Scientist <br>
+                                    Market IQ Inc
+                                </p>
+                                <p>
+                                    2015- 2016	  <br>	
+                                    Senior Data Analyst <br>
+                                    PredictifyMe
+                                </p>
+                                <p>
+                                    2014 – 2015	 <br>
+                                    Idea Engineering Consultant <br>
+                                    Ephlux
+                                </p>
+                                <p>
+                                    <b>Details of Keynote Speeches, Panelist and Chair</b>
+                                    <br>
+                                    <ul>
+                                        <li>
+                                            Guest Speaker session on “Open Archive – Innovating the meter reading process within utilities”, AI Hub, Inqline, 10 April 2021
+                                        </li>
+                                        <li>
+                                            Guest Speaker session on “The World of Predictive Analytics”, Consulting webinar, qordata, 18 December 2021
+                                        </li>
+                                        <li>
+                                            Guest Speaker session on “Release the Breaks & Accelerate Your Journey to AI”, DataCon Pakistan 2019, Marriot Karachi, 14 November 2019
+                                        </li>
+                                        <li>
+                                            Guest Speaker session on “Machine Learning Modelling with ScikitLearn/GraphLab”, Pycon Pakistan 2019, Habib University, Karachi, 12 October 2019
+                                        </li>
+                                        <li>
+                                            8 week long workshop on “Data Science 101”, qordata, 10 October – 28 November, 2018
+                                        </li>
+                                        <li>
+                                            Guest Speaker session on “What it takes to be a Data Scientist”, NED UET, Karachi, 4 August 2016
+                                        </li>
+                                    </ul>
+                                </p>
+                                
+
+                                <h4 class="text-primary">Publications and Research</h4>
+                                <p>
+                                    <ul>
+                                        <li>
+                                            “Python Machine Learning Case Studies”, Apress 2017, ISBN # 9781484228227
+                                        </li>
+                                        <li>
+                                            “Deep generative models to counter class imbalance: a guided model selection strategy”, 2021, IEEE Access, Vol 9, pp. 55879-55897
+                                        </li>
+                                    </ul>
+                                </p>
+                              
+                               
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+
+                    <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq-e-Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br>Accounting & Finance<br>Dean - Faculty of Management Sciences</span>
+
+                            </a>
+                        </div>
+
+                   
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Abdul-Rahim-Nasir.php">Dr. Abdul Rahim Nasir
+                                <span>Professor<br>
+                                Business and Supply Chain Analytics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Faryal-Razzaq.php">Dr. Faryal Razzaq
+                                <span>Assistant Professor<br>Management Sciences</span>
+                            </a>
+                        </div>                     
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Asim-Khwaja.php">Dr. Asim Khwaja
+                                <span>Assistant Professor<br>Decision Sciences</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Sheraz-Mustafa-Rajput.php">Dr. Sheraz Mustafa Rajput
+                                <span>Associate Professor<br> Economics</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waseem-Hassan.php">Dr. Waseem Hassan
+                                <span>Associate Professor<br>Marketing Strategy</span>
+                            </a>
+                        </div>
+
+                       
+
+                        <!-- for visiting faculty members -->
+                        <!-- <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Junaid-Siddiqui.php">Dr. Junaid Siddiqui
+                                <span>Information Systems</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Nausheen-Ahmed.php">Nausheen Ahmed
+                                <span>Business Law</span>
+                            </a>
+                        </div> -->
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
