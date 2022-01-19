32b2d7c37ad323d2b50480bd4d257cec07a0382a
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 16:31:17 +0500

    shahrukh khan profile created.

---
+++ faculty-profile/Mr_Shahrukh_Irfan.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+include('../partials/head.php');
+
+?>
+<style type="text/css">
+body,
+html {
+
+    overflow-x: hidden;
+
+}
+</style>
+
+<body class="mba about bog">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.php" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+
+    #                   MBA Department END                             #
+
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+
+    #                     Faculty Group Start                          #
+
+    #----------------------------------------------------------------->
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="non_faculty_grid">
+                <div class="non_faculty_grid_left">
+                    <div class=" non_faculty_grid_img  non_faculty_grid_img_mr_shahrukh_khan">
+                        <div class="non_faculty_grid_head">
+                            <h1>Mr Shahrukh Irfan</h1>
+                            <h4>Accounting & Finance</h4>
+                        </div>
+                    </div>
+                    <div class="non_faculty_grid_two">
+                        <div class="non_faculty_grid_two_left">
+                            <h4>Recent Activity</h4>
+                            <!-- <div class="recent_activity">
+                                <a href="">Mubashar Speaks on the orientation for the Inaugral batch of KSBL MS Business Analytics. </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href=""><b>Mubashar welcomes Mr. Shoaib Khalil,</b> Head of Channel & Business Development, Microsoft Pakistan, for a session at KSBL on Future Job Trends... </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href="">The board of KSBL has appointed Mr. Mubashar Hameed as the Rector.
+                                    In addition to his role as Rector KSBL, Mr. Hameed will also serve as CEO of Karachi Education Initiative (KEI). </a>
+                                <span>Tuesday, May 12th, 2020</span>
+                            </div> -->
+                        </div>
+                        <div class="non_faculty_grid_two_right">
+                            <div class="non_faculty_grid_two_right_content">
+                                <p><b>Mr Shahrukh Irfan</b> is working as a module leader of PG modules at University of Law Business School. He is also working as an Associate Lecturer – Finance at Coventry Business School at Coventry University. He has more than 20 years’ experience of working in industry and HE institutions.
+                                </p>
+                                <p>
+                                Mr Irfan has taught modules in accounting, finance, economics, and taxation at various institutions both in Pakistan and the UK. He has taught undergraduate, post graduate, MBA and executive MBA cohorts.
+                                </p>
+                                <p>
+                                Mr Irfan is a chartered accountant and a chartered tax advisor and has worked for Big Four firms in London for many years. He has also worked in banking and industry in the area of finance.
+                                </p>
+
+                                <p>
+                                Mr Irfan has a master’s in business economics from the university of the Punjab, Lahore and a master in accounting and finance from London school of economics & political science (LSE).
+                                </p>
+                              
+
+                                <p><h4 class="text-primary">Credentials</h4></p>
+                                <p>
+                                    <b>Education</b>
+                                    <br>
+                                    <ul>
+                                        <li>MSc Accounting and Finance – London School of Economics & Political Science - 2008</li>
+                                        <li>Master of Business Economics (MBEcon) – Department of Economics, University of the Punjab, Lahore - 2001</li>
+                                    </ul>
+                                </p>
+                                <p>
+                                    <b>Professional Qualifications</b>
+                                    <br>
+                                    <ul>
+                                        <li>Chartered Accountant – Institute of Chartered Accountants of Scotland - UK</li>
+                                        <li>Chartered Tax Advisor – Chartered Institute of Taxation - UK</li>
+                                    </ul>
+                                </p>
+
+                                <h4 class="text-primary">Consulting and Teaching</h4>
+                                <p>
+                                    2021 – to date <br>	
+                                    Module Leader – PG accounting & finance modules <br>
+                                    University of Law Business School <br>
+                                    University of Law  <br>
+                                    London
+                                </p>
+                                <p>
+                                    2020 – to date 	 <br>
+                                    Associate Lecturer - Finance <br>
+                                    Coventry University Business School <br>
+                                    Coventry University <br>
+                                    Coventry
+                                </p>
+                                <p>
+                                    2020 – 2021  <br>	
+                                    Senior Lecturer <br>
+                                    LSBU Business School <br>
+                                    London South Bank University <br>
+                                    London
+                                </p>
+                                <p>
+                                    2017 – 2020	 <br>
+                                    Teaching Fellow <br>
+                                    Faculty of Business & Management <br>
+                                    Information Technology University  <br>
+                                    Lahore, Pakistan
+                                </p>
+                                <p>
+                                    2014 - 2017	  <br>	
+                                    Consultant <br>
+                                    International Expatriate Services <br>
+                                    Deloitte LLP <br>
+                                    London
+                                </p>
+                                <p>
+                                    2010 – 2014	 <br>
+                                    Consultant <br>
+                                    International Assignment Services <br>	
+                                    KPMG LLP <br>
+                                    London
+                                </p>
+                                <p>
+                                    2008 - 2010  <br>	
+                                    Assistant Professor <br>
+                                    Forman Business School <br>
+                                    FC College University <br>
+                                    Lahore
+                                </p>
+                                <p>
+                                    2004 – 2007	 <br>
+                                    Manager, Project Finance Division <br>
+                                    PICIC <br>
+                                    Lahore
+                                </p>
+                                <p>
+                                    2001 - 2004 <br>  	
+                                    Finance Officer <br>
+                                    Helicon Pharmaceutek (Pvt.) Ltd <br>
+                                    Lahore
+                                </p>
+                                
+
+                                <!-- <h4 class="text-primary">Publications and Research</h4> -->
+                              
+                               
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+
+                    <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq-e-Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br>Accounting & Finance<br>Dean - Faculty of Management Sciences</span>
+
+                            </a>
+                        </div>
+
+                   
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Abdul-Rahim-Nasir.php">Dr. Abdul Rahim Nasir
+                                <span>Professor<br>
+                                Business and Supply Chain Analytics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Faryal-Razzaq.php">Dr. Faryal Razzaq
+                                <span>Assistant Professor<br>Management Sciences</span>
+                            </a>
+                        </div>                     
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Asim-Khwaja.php">Dr. Asim Khwaja
+                                <span>Assistant Professor<br>Decision Sciences</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Sheraz-Mustafa-Rajput.php">Dr. Sheraz Mustafa Rajput
+                                <span>Associate Professor<br> Economics</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waseem-Hassan.php">Dr. Waseem Hassan
+                                <span>Associate Professor<br>Marketing Strategy</span>
+                            </a>
+                        </div>
+
+                       
+
+                        <!-- for visiting faculty members -->
+                        <!-- <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Junaid-Siddiqui.php">Dr. Junaid Siddiqui
+                                <span>Information Systems</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Nausheen-Ahmed.php">Nausheen Ahmed
+                                <span>Business Law</span>
+                            </a>
+                        </div> -->
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
