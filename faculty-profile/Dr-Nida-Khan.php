5a4609223d542f9ed1579661fa2fe645b07db46a
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 14:47:13 +0500

    dr nida khan content updated

--- faculty-profile/Dr-Nida-Khan.php
+++ faculty-profile/Dr-Nida-Khan.php
@@ -96,159 +96,27 @@ include('../partials/navbar.php');
                         </div>
                         <div class="non_faculty_grid_two_right">
                             <div class="non_faculty_grid_two_right_content">
-                                <p><b>Nida Khan</b> is an Associate Professor of Financial Technology at KSBL. She is also the CEO of Nash fintechX, a Luxembourgish startup, providing software solutions, specializing in blockchain and AI. 
-                                She was ranked as the 12th most influential woman in Islamic business and finance in 2019 and 
-                                2020. Prior to working as the CEO of Nash fintechX, she worked as the CTO of OneAgrix, a Singapore-based e-commerce platform, where she developed their tech stack, gave a concrete architecture to  
-                                their business model and proposed a novel mechanism using blockchain-based tokens to record the 
-                                impact of their initiative. She has worked as a member of SEDAN (Services and Data Management in 
-                                Distributed Systems group) at SnT <i>(Interdisciplinary Center for Security, Reliability and Trust)</i>, 
-                                University of Luxembourg, where she led the research projects of ConexCap, a French-Luxembourgish fintech startup. She has held numerous positions as a faculty member in Islamic 
-                                financial educational institutions in India and has worked as a research assistant on a project 
-                                focusing on performance evaluation of cryptographic protocols in FSTM <i>(Faculty of Science, 
-                                Technology and Medicine),</i> University of Luxembourg.
+                                <p><b>Dr. Nida Khan</b> teaches … at KSBL. She is is the CEO of Nash fintechX, a Luxembourgish startup, providing software solutions and consulting services, specializing in blockchain and AI. 
                                 </p>
                                 <p>
-                                She works as an advisor for iPORTAL Live, World Waqf Day and IBF Net, which are initiatives in the 
-                                Islamic finance space in Singapore, Indonesia and Malaysia, utilising emerging technologies to 
-                                further Islamic financial products and services.
+                                She was ranked as the 12th most influential woman in Islamic business and finance in 2019 and 2020 and is currently working on projects that revolve around impact investing, NFTs for real estate and preventing greenwashing.
                                 </p>
                                 <p>
-                                She holds a doctorate in computer science, where her industrial research on blockchain-based 
-                                financial applications was accorded the prestigious FNR <i>(Luxembourg National Research Fund)</i> grant 
-                                for innovative, industrial projects. She also holds a Master Diploma in Islamic Finance from AIMS, UK 
-                                and is a certified Islamic Expert. She is also a certified Takaful professional and has received VC
-                                training by MiddleGame Ventures, supported by the Luxembourg House of Financial Technology, 
-                                where she emerged as one of the winners of the training program. 
+                                She has worked on projects in social finance and decentralized finance, including tokenization and is a pioneer in giving the economic impact of blockchain-based micropayments, developing a novel consensus mechanism for scalable blockchains, proposing a mathematical framework for blockchain governance, and ensuring GDPR-compliant privacy preservation in blockchains.  
                                 </p>
 
                                 <p>
-                                She has worked on projects in social finance and decentralized finance, including tokenization. She is 
-                                a pioneer in giving the economic impact of blockchain-based micropayments, developing a novel 
-                                consensus mechanism for scalable blockchains, proposing a mathematical framework for blockchain 
-                                governance and ensuring GDPR-compliant privacy preservation in blockchains. Her past 
-                                achievements include developing two pioneering iOS and Android apps for the Islamic finance 
-                                industry, where the latter had 10,000+ installs. Her current projects revolve around impact investing, 
-                                NFTs for real estate and preventing greenwashing. Her work appears in diverse publications and she 
-                                speaks regularly on emerging technologies
-                                </p>
-
-                                <p><h4 class="text-primary">Credentials</h4></p>
-                                
-                                <p>
-                                    <h5><b>Education </b></h5>
-                                    <br>
-                                    <b>PhD Computer Science, University of Luxembourg, Luxembourg</b>
-                                    Topic: Blockchain-enabled Traceability and Immutability for Financial Applications
-                                    <br><br>
-                                    <b>Master in information and Computer Sciences (MICS), University of Luxembourg, Luxembourg</b>
-                                    Master Thesis: Concept and Implementation of an Extension of a Visualisation Tool for the 
-                                    Flowchart-based Visual Satellite Control Language SPELL-Flow
-                                    <br><br>
-                                    <b>Master Diploma in Islamic Finance, AIMS, UK</b>
-                                    Project: Designing an Islamic Financial Product on Salam - Product Manual on Salam
-                                    <br><br>
-                                    <b>Bachelor of Technology, Computer Science & Engineering, UPTU, India</b>
+                                She also worked as the CTO in a Singapore-based e-commerce platform, where she developed their tech stack, gave a concrete architecture to their business model and proposed a novel mechanism using blockchain-based tokens to record the impact of their initiative. Her work appears in diverse publications and she speaks regularly on emerging technologies.
                                 </p>
                                 <p>
-                                <h5><b>Certifications</b></h5>
-                                <br>
-                                <ol>
-                                   <li>VC Scholar Summer Programme: MiddleGame Ventures and Luxembourg House of Financial 
-                                Technology</li>
-                                   <li>Entrepreneurial Women Project: Luxembourg Poland Business Club</li>
-                                   <li>Certified Islamic Finance Expert: AIMS, UK</li>
-                                   <li>Certified Takaful Professional: AIMS, UK</li>
-                                   <li>Statement of Accomplishment in Finance: Professor Kay Giesecke, Stanford University 
-                                    (Venture Lab)</li>
-                                </ol>
-                                </p>
-                                <p>
-                                <h5><b>Awards and Achievements</b></h5>
-
-                                <ol>
-                                <li>Winner of the VC Scholar Programme organized by MiddleGame Ventures and Luxembourg 
-                                House of Financial Technology.</li>
-                                <li>Ranked as the 12th Most Influential Woman in Islamic Business and Finance by Cambridge
-                                IFA, UK in 2019 and 2020.</li>
-                                <li>Developed a mathematical model for blockchain governance based on game theory and 
-                                Nash equilibrium. <a href="https://arxiv.org/abs/2003.09241" target="_Blank">https://arxiv.org/abs/2003.09241</a></li>
-                                <li>Designed and developed a novel privacy management plane to preserve data privacy using 
-                                differential privacy in blockchains and proposed adherence to GDPR. 
-                                <a href="https://youtu.be/RtXJHlGqM2U" target="_Blank">https://youtu.be/RtXJHlGqM2U</a></li>
-                                <li>Won the best workshop award for the paper on the review of privacy-preserving blockchains 
-                                in ACS/ IEEE. </li>
-                                <li>Panelist at The Sukuk Summit held in the European Convention Centre. Invited by the 
-                                Minister of Finance, Luxembourg and President of IsDB to speak on blockchain and sukuk 
-                                tokenization. </li>
-                                <li>Developed a new blockchain consensus mechanism based on MapReduce for high 
-                                performance blockchains. <a href="https://youtu.be/Lw814WelKwU" target="_Blank">https://youtu.be/Lw814WelKwU</a></li>
-                                <li>Developed a light client for the new blockchain platform using IPFS. 
-                                <a href="https://youtu.be/bZPUmtT91yg" target="_Blank">https://youtu.be/bZPUmtT91yg</a></li>
-                                <li>Developed a dashboard to monitor blockchain data and send email notifications to the 
-                                stakeholders. <a href="https://www.youtube.com/watch?v=kA7A9ysvT28&t=2s" target="_Blank">https://www.youtube.com/watch?v=kA7A9ysvT28&t=2s</a></li>
-                                <li>Developed a blockchain-based basic decentralized app for crowdfunding of ethical projects 
-                                by donation. <a href="https://www.youtube.com/watch?v=Bh3Pkg_UU0k" target="_Blank">https://www.youtube.com/watch?v=Bh3Pkg_UU0k</a></li>
-                                <li>Developed the first of its kind Islamic Finance android app (10,000+ installs while it was on 
-                                the Google Play store): 
-                                <a href="https://play.google.com/store/apps/details?id=com.tabrez.khannida.ifinance&hl=en&gl=US" target="_Blank">https://play.google.com/store/apps/details?id=com.tabrez.khannida.ifinance&hl=en&gl=US</a></li>
-                                <li>Developed the world's first Islamic Finance education iOS App.
-                                <a href="https://youtu.be/W83UpEb-x0k" target="_Blank">https://youtu.be/W83UpEb-x0k</a></li>
-                                </ol>
-
-                                </p>
-                                <p>
-                                <h4 class="text-primary">Consulting and Teaching</h4>
-                                <br>
-                                    <ul>
-                                       <li>Advisory Board Member in iPORTAL Live, Malaysia</li>
-                                       <li>Partner in World Waqf Day, Malaysia</li>
-                                       <li>Member in the Experts Panel in IBF Net, Singapore; Indonesia; Malaysia</li>
-                                    </ul>
+                                Dr. Khan holds a doctorate in computer science, from….? where her industrial research on blockchain-based financial applications was accorded the FNR (Luxembourg National Research Fund) grant for innovative, industrial projects.
                                 </p>
 
-                                <p>
-                                <h5><b>Volunteer Work</b></h5>
-                                    <br>
-                                    <ol>
-                                        <li><b>Team Associate in FinTechLab</b> <i>(since 2017 to present).</i> FintechLab is a Canadian observatory focusing on the impact of new financial technology in the financial services industry. The Lab sees itself is a think tank for experts and professionals. <a href="http://fintechlab.ca/team/?lang=en" target="_Blank">http://fintechlab.ca/team/?lang=en</a></li>
-
-                                        <li><b>Team Member of Masdin</b> <i>(February 2017 to July 2020).</i> Masdin is an associate team 
-                                        between the University of Luxembourg and Inria, the French National Institute for computer 
-                                        science and applied mathematics. <a href="https://project.inria.fr/masdin/en/" target="_Blank">https://project.inria.fr/masdin/en/</a></li>
-
-                                        <li><b>Member of Wall Street Blockchain Alliance</b> <i>(April 2019 to April 2020).</i> WSBA is a non-profit trade organization created to guide and promote the adoption of blockchain technology and 
-                                        crypto assets across global financial markets. <a href="https://www.wsba.co/" target="_Blank">https://www.wsba.co/</a></li>
-                                    </ol>
-                                </p>
-                                <p>
-                                    <h5><b>Executive Workshops & Sessions</b></h5>
-                                    <br>
-                                    <ol>
-                                        <li>Conducted executive trainings on the use of blockchain in Islamic Social Finance and Halal 
-                                        Industry for IBF Net, Singapore.</li>
-
-                                        <li>Gave an online talk for the International Conference on blockchain, cryptocurrency and 
-                                        digital assets in Hyderabad, India. <a href="https://youtu.be/EusWnuzsB2o" target="_Blank">https://youtu.be/EusWnuzsB2o</a></li>
-
-                                        <li>Delivered a webinar on blockchain and its applications in the Finance Industry on behalf of 
-                                        the startup, ConexCap organized by Madinah Institute for Leadership and Entrepreneurship. 
-                                        <a href="https://www.youtube.com/watch?v=a1-2mfLVkAw" target="_Blank">https://www.youtube.com/watch?v=a1-2mfLVkAw</a></li>
-
-                                        <li>Delivered a digital strategy session to senior management of Bank of Khartoum, Sudan.</li>
-
-                                        <li>Delivered a workshop on a blockchain-based Zakaat app in the 5th International Conference 
-                                        on Entrepreneurial Finance, CIFEMA 2017, Morocco.</li>
-                                    </ol>
-
-                                </p>
-                                <p>
-                                    <h4 class="text-primary">Publications and Research</h4>
-                                    <ol>
-                                        <li>The complete list of academic publications can be accessed from: 
-                                         <a href="https://www.researchgate.net/profile/Nida-Khan-7/research" target="_Blank">https://www.researchgate.net/profile/Nida-Khan-7/research</a></li>
-                                        <li>Link to the GitHub repositories: <a href="https://github.com/nidakhanlu?tab=repositories" target="_Blank">https://github.com/nidakhanlu?tab=repositories</a></li>
-                                    </ol>
-                                </p>
+                                <!-- <p><h4 class="text-primary">Credentials</h4></p> -->
+                                <!-- <h4 class="text-primary">Consulting and Teaching</h4> -->
+                                <!-- <h4 class="text-primary">Publications and Research</h4> -->
+                              
+                               
 
                             </div>
                         </div>
