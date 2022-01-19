b109fbf55d7b0b5498f03a5f2e885fb24af97cf6
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 21:11:20 +0500

    Amra mubashir profile created.

---
+++ faculty-profile/Amra_Mubashir.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+include('../partials/head.php');
+
+?>
+<style type="text/css">
+body,
+html {
+
+    overflow-x: hidden;
+
+}
+</style>
+
+<body class="mba about bog">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.php" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+
+    #                   MBA Department END                             #
+
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+
+    #                     Faculty Group Start                          #
+
+    #----------------------------------------------------------------->
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="non_faculty_grid">
+                <div class="non_faculty_grid_left">
+                    <div class=" non_faculty_grid_img  non_faculty_grid_img_mr_shahrukh_khan">
+                        <div class="non_faculty_grid_head">
+                            <h1>Amra Mubashir</h1>
+                            <h4>Head of HR, Diversity Expert, Talent Management</h4>
+                        </div>
+                    </div>
+                    <div class="non_faculty_grid_two">
+                        <div class="non_faculty_grid_two_left">
+                            <h4>Recent Activity</h4>
+                            <!-- <div class="recent_activity">
+                                <a href="">Mubashar Speaks on the orientation for the Inaugral batch of KSBL MS Business Analytics. </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href=""><b>Mubashar welcomes Mr. Shoaib Khalil,</b> Head of Channel & Business Development, Microsoft Pakistan, for a session at KSBL on Future Job Trends... </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href="">The board of KSBL has appointed Mr. Mubashar Hameed as the Rector.
+                                    In addition to his role as Rector KSBL, Mr. Hameed will also serve as CEO of Karachi Education Initiative (KEI). </a>
+                                <span>Tuesday, May 12th, 2020</span>
+                            </div> -->
+                        </div>
+                        <div class="non_faculty_grid_two_right">
+                            <div class="non_faculty_grid_two_right_content">
+                                <p><b>Amra Mubashir</b>
+                                 is an HR professional having work experience of more than 15 years with multinational and local companies. She has expertise in leadership development, diversity, and inclusion. Amra has coached young professionals across Asia Pacific particularly Pakistan, Thailand, Vietnam, Philippines, Malaysia and Indonesia. She has taught in FAST-NU and wrote local case studies for LUMS which have been published by LUMS in a book on Knitwear Industry of Pakistan. Previously Amra has worked in PepsiCo International, British American Tobacco, Jazz and Tetra Pak in local and Regional roles. Currently she is heading Human Resources at Tapal tea (PVT) Ltd.
+                                </p>
+
+                                
+                                <p>
+                                She is also a co-founder of a female community, Her Bright Beginning, a platform for all women creating a broad range of opportunities that inspires and enables women of all walks of life. She has been a key note and conference speaker for many salient conferences within Pakistan and abroad. Amra has worked with Global Centre of Inclusion, USA on "DEI Futures to reinvent, reimagine and retool D&I practices".
+                                </p>
+                                <p>
+                                Amra holds Masters in Engineering Business Management from Warwick University, UK with majors in change management and HR. She has bachelor’s degree in Computer Sciences from University of Engineering and Technology, Lahore. She holds D&I certification from INSEAD Business School.
+                                </p>
+                               
+
+                                <p><h4 class="text-primary">Credentials</h4></p>
+                                <p>
+                                    <b>Education</b>  
+                                </p>
+
+                                <p>
+                                    <b>M.Sc. (Engineering Business Management)</b> <br>
+                                    University of Warwick  <br>
+                                    2004 - 2005
+                                </p>
+                                <p>
+                                    <b>B.Sc. (Honors, Computer Science)</b> <br>
+                                    University of Engineering and Technology, Lahore <br>
+                                    1999 - 2002
+                                </p>
+                                <p>
+                                    <b>Licenses & certifications</b>
+                                </p>
+                                <p>
+                                    GIST U.S. - Pakistan Start up Training <br>
+                                    Department of State
+                                </p>
+                                <p>
+                                    INSEAD Gender Diversity Program <br>
+                                    INSEAD Executive Education
+                                </p>
+
+                                <!-- <h4 class="text-primary">Consulting and Teaching</h4> -->
+                                <!-- <h4 class="text-primary">Publications and Research</h4> -->
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+
+                    <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq-e-Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br>Accounting & Finance<br>Dean - Faculty of Management Sciences</span>
+
+                            </a>
+                        </div>
+
+                   
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Abdul-Rahim-Nasir.php">Dr. Abdul Rahim Nasir
+                                <span>Professor<br>
+                                Business and Supply Chain Analytics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Faryal-Razzaq.php">Dr. Faryal Razzaq
+                                <span>Assistant Professor<br>Management Sciences</span>
+                            </a>
+                        </div>                     
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Asim-Khwaja.php">Dr. Asim Khwaja
+                                <span>Assistant Professor<br>Decision Sciences</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Sheraz-Mustafa-Rajput.php">Dr. Sheraz Mustafa Rajput
+                                <span>Associate Professor<br> Economics</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waseem-Hassan.php">Dr. Waseem Hassan
+                                <span>Associate Professor<br>Marketing Strategy</span>
+                            </a>
+                        </div>
+
+                       
+
+                        <!-- for visiting faculty members -->
+                        <!-- <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Junaid-Siddiqui.php">Dr. Junaid Siddiqui
+                                <span>Information Systems</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Nausheen-Ahmed.php">Nausheen Ahmed
+                                <span>Business Law</span>
+                            </a>
+                        </div> -->
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
