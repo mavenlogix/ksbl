15f9bac4c37c4d7c63d8a346c1765356c5db29e9
sajidkhattak1996 <dev2.maven@gmail.com>
2022-01-19 20:44:39 +0500

    Absar ahmed profile created

---
+++ faculty-profile/Mr_Absar_Ahmed.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+include('../partials/head.php');
+
+?>
+<style type="text/css">
+body,
+html {
+
+    overflow-x: hidden;
+
+}
+</style>
+
+<body class="mba about bog">
+    <!-----------------------------------------------------------------#
+
+    #                       Header START                               #
+
+    #----------------------------------------------------------------->
+    <header id="header" class="run">
+        <div class="container">
+            <?php
+include('../partials/navbar.php'); 
+?>
+
+            <div class="header-content">
+                <a href="../index.php" title="KSBL" class="logo">
+                    <img src="../images/ksbl-logo-high.png" width="239" alt="KSBL" />
+                </a>
+                <div class="heder-info">
+                    <h1>FACULTY</h1>
+                    <h2>KSBL faculty is composed of world-class scholars, focused on <span class="orange-yellow-text">transferring their knowledge & wisdom</span> to the next generation</h2>
+                </div><!-- container -->
+            </div>
+        </div><!-- container -->
+        </div><!-- Header-bottom -->
+    </header>
+    <!-----------------------------------------------------------------#
+
+    #                       Header END                                 #
+
+    #----------------------------------------------------------------->
+    <div class="departments">
+        <div class="container">
+            <a href="../about/" class="department" title="Vision & Mission">Vision & Mission</a>
+            <!-- About -->
+            <a href="../about/board-of-governors.php" class="department " title="Board of Governors">Board of Governors</a>
+            <!-- Board of Governors -->
+            <a href="../faculty.php" class="department" title="Faculty">Faculty</a>
+            <!-- Accreditation -->
+            <a href="../about/karachi-education-initiative/" class="department" title="KEI">KEI</a>
+            <!-- KEi -->
+            <a href="../contact-us.php" class="department" title="Contact Us">Contact Us</a>
+            <!-- Contact -->
+        </div><!-- container -->
+    </div>
+    <!-----------------------------------------------------------------#
+
+    #                   MBA Department END                             #
+
+    #----------------------------------------------------------------->
+    <!-----------------------------------------------------------------#
+
+    #                     Faculty Group Start                          #
+
+    #----------------------------------------------------------------->
+    <div class="faculty-section teacher-group">
+        <div class="container">
+            <div class="non_faculty_grid">
+                <div class="non_faculty_grid_left">
+                    <div class=" non_faculty_grid_img  non_faculty_grid_img_mr_shahrukh_khan">
+                        <div class="non_faculty_grid_head">
+                            <h1>Mr Absar Ahmed</h1>
+                            <h4>Lecturer in College Algebra</h4>
+                        </div>
+                    </div>
+                    <div class="non_faculty_grid_two">
+                        <div class="non_faculty_grid_two_left">
+                            <h4>Recent Activity</h4>
+                            <!-- <div class="recent_activity">
+                                <a href="">Mubashar Speaks on the orientation for the Inaugral batch of KSBL MS Business Analytics. </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href=""><b>Mubashar welcomes Mr. Shoaib Khalil,</b> Head of Channel & Business Development, Microsoft Pakistan, for a session at KSBL on Future Job Trends... </a>
+                                <span>Monday, March 22nd, 2021</span>
+                            </div>
+                            <div class="recent_activity">
+                                <a href="">The board of KSBL has appointed Mr. Mubashar Hameed as the Rector.
+                                    In addition to his role as Rector KSBL, Mr. Hameed will also serve as CEO of Karachi Education Initiative (KEI). </a>
+                                <span>Tuesday, May 12th, 2020</span>
+                            </div> -->
+                        </div>
+                        <div class="non_faculty_grid_two_right">
+                            <div class="non_faculty_grid_two_right_content">
+                                <p><b>Mr. Absar Ahmed</b> is a Lecturer of Mathematics at KSBL. He has over 14 years of experience in academia.
+                                </p>
+                                <p>
+                                In his academia experience, Mr. Ahmed has taught at various reputed universities & colleges including National University of Sciences and Technology (PNEC), DHA Suffa University, Dadabhoy Institute of Higher Education and  Sir Adamjee Intermediate College, Bahria foundation College. 
+                                </p>
+                                <p>
+                                He has also served as Head of Undergraduate program at NUST PNEC and Member of Discipline in Sir Adamjee Intermediate College, Bahria foundation College
+                                </p>
+                                <p>
+                                Mr. Ahmed completed his M.Phil. from Federal Urdu University, Karachi, Pakistan and published five papers in national and international journals
+                                </p>
+
+                              
+
+                                <p><h4 class="text-primary">Credentials</h4></p>
+                                <p>
+                                    <b>Education</b>
+                                    <br>
+                                    M. Phil in Mathematics (Applied) from Federal Urdu University, 2012 – 2018 <br>
+                                    M.Sc. in Mathematics (2nd Position) from University of Karachi  2009 – 2010 <br>
+                                    B. Sc. in Maths with Phy. and Chem. From University of Karachi 2005 - 2007
+                                </p>
+                                <p>
+                                    <b>Professional Trainings</b>
+                                    <br>
+                                    <ul>
+                                        <li>
+                                            The Integrated Personality Development Course (IPDC)
+                                            Karachi , Pakistan
+                                        </li>
+                                    </ul>
+                                    
+                                </p>
+                                <br>
+                            
+                                
+
+                                <h4 class="text-primary">Consulting and Teaching</h4>
+                                <p>
+                                    <b>Permanent Faculty</b>
+                                    <ul>
+                                    <li>
+                                        <a href="https://www.google.com/search?q=National+University+of+Sciences+and+Technology&stick=H4sIAAAAAAAAAOPgE-LUz9U3sMzJSilUAjONK4zMLLSMspOt9FNTSpMTSzLz8xCsxJz4zLziksySUhDPKjEtLTMnEyxTDACPwpGNTAAAAA&sa=X&ved=2ahUKEwiE_KLX1rzfAhUFK48KHZD2D8wQmxMoATAdegQIERAD" style="color: black;text-decoration: underline">National University of Sciences and Technology (PNEC)</a>
+                                        Lecturer in Mathematics Sep 2018 to Present 
+                                    </li>
+
+                                    <li>
+                                        Sir Adamjee Intermediate College
+                                        Lecturer in Mathematics <i>Aug 2012 – Sep 2018</i>
+                                    </li>
+
+                                        Bahria foundation College 
+                                        Lecturer in Mathematics <i>Feb - 2011 to Aug – 2012</i>
+                                    </li>
+
+
+                                    <li>
+                                        Karachi Generations School
+                                        Math Teacher (CAIE) <i>Feb - 2007 to Nov – 2008</i>
+                                    </li>
+                                    </ul>
+                                </p>
+
+                                <p>
+                                    <b>Visiting Faculty</b>
+                                    <br>
+                                    <ul>
+                                        <li>
+                                            Dadabhoy Institute of Higher Edu. <br>
+                                            Lecturer in Mathematics <br>
+                                            June - 2017 to Aug 2018
+                                        </li>
+
+                                        <li>
+                                            DHA Suffa university <br>
+                                            Lecturer in Mathematics <br>
+                                            Oct 2020 to Feb 2021
+                                        </li>
+
+                                        <li>
+                                            Karachi School of Business and Leadership <br>
+                                            Lecturer in Mathematics <br>
+                                            Oct 2021 to Feb 2022
+                                        </li>
+                                    </ul>
+
+                                </p>
+                               
+
+                                <h4 class="text-primary">Publications and Research</h4>
+                                <p>
+                                    <ol>
+                                        <li>
+                                            Saqib Ur Rehman†, Bilal Ahmed Usmani, Kamran Khan, Abdul Jamil Khan, Mustafain Ali, <b>Absar Ahmed</b> and Sara Ali(2019), Subtropical anticyclone tracking using Principal Component Analysis, <i>International Journal of Computer Science and Network Security,VOL.19 No.4, April 2019.</i>
+                                        </li>
+
+                                        <li>
+                                            Saqib Ur Rehman†, Bilal Ahmed Usmani, Kamran Khan, Abdul Jamil Khan, Mustafain Ali, <b>Absar Ahmed</b> and Sara Ali(2019), Wavelet Analysis for precipitation attributes, <i>
+                                            International Journal of Computer Science and Network Security,VOL.19 No.4, April 2019.
+                                            </i>
+                                        </li>
+
+                                        <li>
+                                            Saqib Ur Rehman, Kashif Saleem, Asif Masood, <b>Absar Ahmed</b>, Abdul Jamil Khan,(2015), Runoff Variability assessment for Victoria:  A Case Study over Goulburn River. <i>Research journal of fisheries and hydrobiology, 10(12),14-18. Published: 5th October 2015. Published: 24th July 2015.</i>
+                                        </li>
+
+                                        <li>
+                                            Saqib Ur Rehman, Kamran Khan and <b>Absar Ahmed</b>, (2015), Association between the Dynamics of Indian Ocean Subtropical High and winter time precipitation and Stream flow: A Case Study over Acheron River Catchment, Victoria, <i>British Journal of Applied Science & Technology, 12(3), 1-9. Published: 5th October 2015.</i>
+                                        </li>
+
+                                        <li>
+                                            Saqib Ur Rehman, Kashif Saleem, <b>Absar Ahmed</b> and Muhammad Abdul Basit, (2015), Relationship between Seasonal Streamflow Variability and their Evolution through Time: A Case Study over Avoca River Catchment, <i>Journal of Scientific Research & Reports, 9(2). 1-6. Published: 8th October 2015.</i>
+                                        </li>
+
+                                        <li>
+                                            Saqib Ur Rehman, Kashif Saleem and <b>Absar Ahmed</b>, (2015), Quantifying Indian Ocean Subtropical High-runoff Relationships a Case Study over Campaspe River, <i>Advances in Research, 6(2),1-9. Published: 3rd November 2015.</i>
+                                        </li>
+                                    </ol>
+                                </p>
+                              
+                               
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="non_faculty_grid_right">
+                <h4>Other Faculty Members</h4>
+                    <div class="non_faculty_grid_right_scroll">
+
+                    <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Farooq-e-Azam-Cheema.php">Dr. Farooq-e-Azam Cheema
+                                <span>Provost & Acting Registrar</span>
+
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Tariq-Basheer.php">Dr. Tariq Basheer
+                                <span>Professor<br>Accounting & Finance<br>Dean - Faculty of Management Sciences</span>
+
+                            </a>
+                        </div>
+
+                   
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Nadeem-Javaid.php">Dr. Nadeem Javaid
+                                <span>Professor<br>
+                                    Strategy & Economics<br>
+                                </span>
+                            </a>
+                        </div>
+                        
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Abdul-Rahim-Nasir.php">Dr. Abdul Rahim Nasir
+                                <span>Professor<br>
+                                Business and Supply Chain Analytics<br>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Shazib-Ehsan-Shaikh.php">Dr. Shazib Ehsan Shaikh
+                                <span>Associate Professor,<br>
+                                    Business Analytics<br>
+                                    Head of Department, Decision Sciences
+                                </span>
+                            </a>
+                        </div>
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Ahmad-Junaid.php">Dr. Ahmad Junaid
+                                <span>Associate Professor<br>
+                                    Finance & Accounting
+                                </span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Muhammad-Athar-Siddiqui.php">Dr. Athar Siddiqui
+                                <span>Associate Professor<br>
+                                    OB & HR</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Faryal-Razzaq.php">Dr. Faryal Razzaq
+                                <span>Assistant Professor<br>Management Sciences</span>
+                            </a>
+                        </div>                     
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Asim-Khwaja.php">Dr. Asim Khwaja
+                                <span>Assistant Professor<br>Decision Sciences</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Sheraz-Mustafa-Rajput.php">Dr. Sheraz Mustafa Rajput
+                                <span>Associate Professor<br> Economics</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Waseem-Hassan.php">Dr. Waseem Hassan
+                                <span>Associate Professor<br>Marketing Strategy</span>
+                            </a>
+                        </div>
+
+                       
+
+                        <!-- for visiting faculty members -->
+                        <!-- <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Dr-Junaid-Siddiqui.php">Dr. Junaid Siddiqui
+                                <span>Information Systems</span>
+                            </a>
+                        </div>
+
+                        <div class="non_faculty_grid_right_sub_head">
+                            <a href="faculty-profile/Nausheen-Ahmed.php">Nausheen Ahmed
+                                <span>Business Law</span>
+                            </a>
+                        </div> -->
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <?php
+include('../partials/footer.php');
+?>
+
+<!-- footer end -->
+<?php
+include('../partials/footer-scripts.php');
+?>
+
+
+</body>
+
+</html>
